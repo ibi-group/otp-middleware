b898e638e379c9e97f398d6492118c92a50460f7
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2020-02-26 13:18:37 -0500

    feat(spark-swagger): Add spark-swagger documentation to endpoints.

--- src/main/java/org/opentripplanner/middleware/controllers/api/ApiController.java
+++ src/main/java/org/opentripplanner/middleware/controllers/api/ApiController.java
@@ -1,8 +1,12 @@
 package org.opentripplanner.middleware.controllers.api;
 
+import com.beerboy.spark.typify.route.GsonRoute;
+import com.beerboy.ss.SparkSwagger;
+import com.beerboy.ss.rest.Endpoint;
 import org.eclipse.jetty.http.HttpStatus;
 import org.opentripplanner.middleware.models.Model;
 import org.opentripplanner.middleware.persistence.TypedPersistence;
+import org.opentripplanner.middleware.spark.Main;
 import org.opentripplanner.middleware.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +17,9 @@ import spark.Response;
 import java.util.Date;
 import java.util.List;
 
+import static com.beerboy.ss.descriptor.EndpointDescriptor.endpointPath;
+import static com.beerboy.ss.descriptor.MethodDescriptor.path;
+import static com.beerboy.ss.rest.RestResponse.ok;
 import static org.opentripplanner.middleware.utils.JsonUtils.getPOJOFromRequestBody;
 import static org.opentripplanner.middleware.utils.JsonUtils.logMessageAndHalt;
 import static spark.Spark.delete;
@@ -30,7 +37,7 @@ import static spark.Spark.put;
  *  that exist in other collection).
  * @param <T> One of the {@link Model} classes (extracted from {@link TypedPersistence})
  */
-public abstract class ApiController<T extends Model> {
+public abstract class ApiController<T extends Model> implements Endpoint {
     private static final String ID_PARAM = "/:id";
     private final String ROOT_ROUTE;
     private static final String SECURE = "secure/";
@@ -48,7 +55,75 @@ public abstract class ApiController<T extends Model> {
         this.persistence = persistence;
         this.classToLowercase = persistence.clazz.getSimpleName().toLowerCase();
         this.ROOT_ROUTE = apiPrefix + SECURE + classToLowercase;
-        registerRoutes();
+        if (!Main.DO_SWAGGER) registerRoutes();
+    }
+
+    /**
+     * This method is called by spark-swagger to register endpoints and generate the docs.
+     * @param restApi The object to which to attach the documentation.
+     */
+    @Override
+    public void bind(final SparkSwagger restApi) {
+        LOG.info("Registering routes and enabling docs for {}", ROOT_ROUTE);
+
+        restApi.endpoint(endpointPath(ROOT_ROUTE)
+                .withDescription("Interface for querying and managing '" + classToLowercase + "' entities."), (q, a) -> LOG.info("Received request for '{}' Rest API", classToLowercase))
+
+
+                // If using lambdas with the GET method, a bug in the documentation framework
+                // requires you to write path(<entire_route>).
+                // If you use new GsonRoute() {...} with the GET method, you only need to write path(<relative_to_endpointPath>).
+                // Other HTTP methods are not affected by this bug.
+
+                // Get multiple entities.
+                .get(path(ROOT_ROUTE)
+                        .withDescription("Gets a list of all '" + classToLowercase + "' entities.")
+                        .withResponseAsCollection(clazz)
+                        ,
+                        this::getMany, JsonUtils::toJson
+                )
+
+                // Get one entity.
+                .get(path(ROOT_ROUTE + ID_PARAM)
+                        .withDescription("Returns a '" + classToLowercase + "' entity with the specified id, or 404 if not found.")
+                        .withPathParam().withName("id").withDescription("The id of the entity to search.").and()
+                        .withResponseType(clazz)
+                        // .withResponses(...) // FIXME: not implemented (requires source change).
+                        ,
+                        this::getOne, JsonUtils::toJson
+                )
+
+                // Options request.
+                .options(path(""), (req, res) -> "")
+
+                // Create entity request
+                .post(path("")
+                        .withDescription("Creates a '" + classToLowercase + "' entity.")
+                        .withRequestType(clazz) // FIXME: Embedded Swagger UI doesn't work for this request. (Embed or link a more recent version?)
+                        .withResponseType(clazz)
+                        ,
+                        this::createOrUpdate, JsonUtils::toJson
+                )
+
+                // Update entity request
+                .put(path(ID_PARAM)
+                        .withDescription("Updates and returns the '" + classToLowercase + "' entity with the specified id, or 404 if not found.")
+                        .withPathParam().withName("id").withDescription("The id of the entity to update.").and()
+                        .withRequestType(clazz) // FIXME: Embedded Swagger UI doesn't work for this request. (Embed or link a more recent version?)
+                        .withResponseType(clazz)
+                        // .withResponses(...) // FIXME: not implemented (requires source change).
+                        ,
+                        this::createOrUpdate, JsonUtils::toJson
+                )
+
+                // Delete entity request
+                .delete(path(ID_PARAM)
+                        .withDescription("Deletes the '" + classToLowercase + "' entity with the specified id if it exists.")
+                        .withPathParam().withName("id").withDescription("The id of the entity to delete.").and()
+                        .withGenericResponse()
+                        ,
+                        this::deleteOne, JsonUtils::toJson
+                );
     }
 
     /**
@@ -73,14 +148,14 @@ public abstract class ApiController<T extends Model> {
     /**
      * HTTP endpoint to get multiple entities.
      */
-    private List getMany(Request req, Response res) {
+    private List<T> getMany(Request req, Response res) {
         return persistence.getAll();
     }
 
     /**
      * HTTP endpoint to get one entity specified by ID.
      */
-    private Model getOne(Request req, Response res) {
+    private T getOne(Request req, Response res) {
         String id = getIdFromRequest(req);
         return getObjectForId(req, id);
     }
@@ -112,8 +187,8 @@ public abstract class ApiController<T extends Model> {
     /**
      * Convenience method for extracting the ID param from the HTTP request.
      */
-    private Model getObjectForId(Request req, String id) {
-        Model object = persistence.getById(id);
+    private T getObjectForId(Request req, String id) {
+        T object = (T) persistence.getById(id);
         if (object == null) {
             logMessageAndHalt(
                 req,
@@ -130,7 +205,7 @@ public abstract class ApiController<T extends Model> {
      * PUT, an update operation will be applied to the specified entity using the JSON body found in the request.
      * Otherwise, a new entity will be created.
      */
-    private Model createOrUpdate(Request req, Response res) {
+    private T createOrUpdate(Request req, Response res) {
         long startTime = System.currentTimeMillis();
         // Check if an update or create operation depending on presence of id param
         // This needs to be final because it is used in a lambda operation below.
@@ -157,7 +232,7 @@ public abstract class ApiController<T extends Model> {
                 persistence.replace(id, object);
             }
             // Return object that ultimately gets stored in database.
-            return persistence.getById(object.id);
+            return (T) persistence.getById(object.id);
         } catch (HaltException e) {
             throw e;
         } catch (Exception e) {
--- src/main/java/org/opentripplanner/middleware/spark/Main.java
+++ src/main/java/org/opentripplanner/middleware/spark/Main.java
@@ -1,48 +1,95 @@
 package org.opentripplanner.middleware.spark;
 
+import com.beerboy.ss.SparkSwagger;
 import org.opentripplanner.middleware.BasicOtpDispatcher;
 import org.opentripplanner.middleware.controllers.api.ApiControllerImpl;
 import org.opentripplanner.middleware.persistence.Persistence;
+import spark.Service;
+
+import java.io.IOException;
+import java.util.List;
 
 import static spark.Spark.*;
 
 public class Main {
-    private static final String API_PREFIX = "api/";
+    private static final String API_PREFIX = "/api/";
+    public static final boolean DO_SWAGGER = true;
 
-    public static void main(String[] args) {
-        // Connect to the MongoDB
+    public static void main(String[] args) throws IOException {
+        // Connect to MongoDB.
         Persistence.initialize();
-        // Define some endpoints,
-        staticFileLocation("/public");
 
-        //
-        // websocket() must be declared before the other get() endpoints.
-        // available at http://localhost:4567/async-websocket
-        webSocket("/async-websocket", BasicOtpWebSocketController.class);
+        if (DO_SWAGGER) {
+            // Must start spark explicitly to use spark-swagger.
+            // https://github.com/manusant/spark-swagger#endpoints-binding
+            Service spark = Service.ignite().port(Service.SPARK_DEFAULT_PORT);
 
-        // available at http://localhost:4567/hello
-        get("/hello", (req, res) -> "(Sparks) OTP Middleware says Hi!");
+            // Define some endpoints.
+            // spark.staticFileLocation("/public");
 
-        // available at http://localhost:4567/sync
-        get("/sync", (req, res) -> BasicOtpDispatcher.executeRequestsInSequence());
+            // websocket() must be declared before the other get() endpoints.
+            // available at http://localhost:4567/async-websocket
+            spark.webSocket("/async-websocket", BasicOtpWebSocketController.class);
 
-        // available at http://localhost:4567/async
-        get("/async", (req, res) -> BasicOtpDispatcher.executeRequestsAsync());
 
-        // Register API routes.
-        new ApiControllerImpl(API_PREFIX, Persistence.users);
-        // TODO Add other models.
+            SparkSwagger.of(spark)
+                    // Register API routes.
+                    .endpoints(() -> List.of(
+                            new ApiControllerImpl(API_PREFIX, Persistence.users)
+                            // TODO Add other models.
+                    ))
+                    .generateDoc();
 
-        before(API_PREFIX + "secure/*", ((request, response) -> {
-            // TODO Add Auth0 authentication to requests.
+            // available at http://localhost:4567/hello
+            spark.get("/", (req, res) -> "(Sparks) OTP Middleware says Hi!");
+
+            // available at http://localhost:4567/sync
+            spark.get("/sync", (req, res) -> BasicOtpDispatcher.executeRequestsInSequence());
+
+            // available at http://localhost:4567/async
+            spark.get("/async", (req, res) -> BasicOtpDispatcher.executeRequestsAsync());
+
+            spark.before(API_PREFIX + "secure/*", ((request, response) -> {
+                // TODO Add Auth0 authentication to requests.
 //            Auth0Connection.checkUser(request);
 //            Auth0Connection.checkEditPrivileges(request);
-        }));
+            }));
 
-        // Return "application/json" and set gzip header for all API routes.
-        before(API_PREFIX + "*", (request, response) -> {
-            response.type("application/json");
-            response.header("Content-Encoding", "gzip");
-        });
+            // Return "application/json" and set gzip header for all API routes.
+            spark.before(API_PREFIX + "*", (request, response) -> {
+                response.type("application/json"); // Handled by API response documentation. If specified, "Try it out" feature in API docs fails.
+                response.header("Content-Encoding", "gzip");
+            });
+        }
+        else {
+            // websocket() must be declared before the other get() endpoints.
+            // available at http://localhost:4567/async-websocket
+            webSocket("/async-websocket", BasicOtpWebSocketController.class);
+
+            // available at http://localhost:4567/hello
+            get("/", (req, res) -> "(Sparks) OTP Middleware says Hi!");
+
+            // available at http://localhost:4567/sync
+            get("/sync", (req, res) -> BasicOtpDispatcher.executeRequestsInSequence());
+
+            // available at http://localhost:4567/async
+            get("/async", (req, res) -> BasicOtpDispatcher.executeRequestsAsync());
+
+            // Register API routes.
+            new ApiControllerImpl(API_PREFIX, Persistence.users);
+            // // TODO Add other models.
+
+            before(API_PREFIX + "secure/*", ((request, response) -> {
+                // TODO Add Auth0 authentication to requests.
+    //            Auth0Connection.checkUser(request);
+    //            Auth0Connection.checkEditPrivileges(request);
+            }));
+
+            // Return "application/json" and set gzip header for all API routes.
+            before(API_PREFIX + "*", (request, response) -> {
+                response.type("application/json"); // Handled by API response documentation. If specified, "Try it out" feature in API docs fails.
+                response.header("Content-Encoding", "gzip");
+            });
+        }
     }
 }
