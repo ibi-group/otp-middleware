b5c92e34107cb90dd9c4c6ca4d1e6655e556b8f0
Rob Beer <rbeer@ibigroup.com>
2020-05-22 14:57:22 +0100

    Added maximum monitored trips check

--- src/main/java/org/opentripplanner/middleware/controllers/api/MonitorTripController.java
+++ src/main/java/org/opentripplanner/middleware/controllers/api/MonitorTripController.java
@@ -1,13 +1,17 @@
 package org.opentripplanner.middleware.controllers.api;
 
+import com.mongodb.client.model.Filters;
+import org.bson.conversions.Bson;
 import org.eclipse.jetty.http.HttpStatus;
 import org.opentripplanner.middleware.models.MonitoredTrip;
 import org.opentripplanner.middleware.models.User;
 import org.opentripplanner.middleware.persistence.Persistence;
+import org.opentripplanner.middleware.utils.HttpUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
 
+import static com.mongodb.client.model.Filters.eq;
 import static org.opentripplanner.middleware.utils.JsonUtils.logMessageAndHalt;
 
 /**
@@ -25,6 +29,7 @@ public class MonitorTripController extends ApiController<MonitoredTrip> {
     MonitoredTrip preCreateHook(MonitoredTrip monitoredTrip, Request req) {
         // Confirm that the user exists before creating monitored trip
         isValidUser(monitoredTrip.userId, req);
+        reachedMaximum(monitoredTrip.userId, req);
 
         return monitoredTrip;
     }
@@ -44,10 +49,22 @@ public class MonitorTripController extends ApiController<MonitoredTrip> {
 
     // Confirm that the user exists before creating monitored trip
     private void isValidUser(String userId, Request req) {
-        User user = Persistence.users.getById(userId);
-        if (user == null) {
-            LOG.error("User with id {} does not exist", userId);
-            logMessageAndHalt(req, HttpStatus.BAD_REQUEST_400, "User does not exist.");
+//        Auth0Connection.checkUser(request);
+//        Auth0UserProfile requestingUser = Auth0Connection.getUserFromRequest(request);
+//        User user = Persistence.users.getById(userId);
+//        if (requestingUser.user_id != user.auth0UserId) {
+//            logMessageAndHalt(request, HttpStatus.FORBIDDEN_403, "Can only obtain trip requests created by the same user.");
+//        }
+    }
+
+    private void reachedMaximum(String userId, Request request) {
+        long maximum = 5;
+
+        // filter monitored trip on user id to find out how many have already been saved
+        Bson filter = Filters.and(eq("userId", userId));
+        long count = this.persistence.getCountFiltered(filter);
+        if (count == maximum) {
+            logMessageAndHalt(request, HttpStatus.BAD_REQUEST_400, "Maximum amount of saved monitored trips reached. Maximum = " + maximum);
         }
     }
 }
