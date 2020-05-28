3576aa4aacdadba46e72fd27a879bf1cff5a1281
Landon Reed <landontreed@gmail.com>
2020-05-28 13:03:12 -0400

    refactor: add javadoc

--- src/main/java/org/opentripplanner/middleware/models/ApiUser.java
+++ src/main/java/org/opentripplanner/middleware/models/ApiUser.java
@@ -3,6 +3,10 @@ package org.opentripplanner.middleware.models;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a third-party application developer, which has a set of AWS API Gateway API keys which they can use to
+ * access otp-middleware's endpoints (as well as the geocoding and OTP endpoints).
+ */
 public class ApiUser extends AbstractUser {
     public List<String> apiKeyIds = new ArrayList<>();
 }
--- src/main/java/org/opentripplanner/middleware/models/OtpUser.java
+++ src/main/java/org/opentripplanner/middleware/models/OtpUser.java
@@ -1,5 +1,10 @@
 package org.opentripplanner.middleware.models;
 
+/**
+ * This represents a user of an OpenTripPlanner instance (typically of the standard OTP UI/otp-react-redux).
+ * otp-middleware stores these users and associated information (e.g., home/work locations and other favorites). Users
+ * can also opt-in to storing their trip planning requests/responses.
+ */
 public class OtpUser extends AbstractUser {
     // FIXME: Binh to migrate OTP user fields here.
 }
