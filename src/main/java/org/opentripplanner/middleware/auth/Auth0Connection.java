4d7032e87ee70f46a07c1f59e3ca71891b7d8df7
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2020-05-28 10:28:10 -0400

    refactor(Auth0Connection): Remove unused method.

--- src/main/java/org/opentripplanner/middleware/auth/Auth0Connection.java
+++ src/main/java/org/opentripplanner/middleware/auth/Auth0Connection.java
@@ -1,6 +1,5 @@
 package org.opentripplanner.middleware.auth;
 
-import com.auth0.json.auth.TokenHolder;
 import com.auth0.jwk.Jwk;
 import com.auth0.jwk.JwkException;
 import com.auth0.jwk.JwkProvider;
@@ -146,12 +145,5 @@ public class Auth0Connection {
      */
     public static boolean authDisabled() {
         return hasConfigProperty("DISABLE_AUTH") && "true".equals(getConfigPropertyAsText("DISABLE_AUTH"));
-    }
-
-    /**
-     * Computes a token expiration time in seconds.
-     */
-    public static long getTokenExpirationTime(TokenHolder token) {
-        return System.currentTimeMillis() + token.getExpiresIn() /* seconds */ * 1000;
     }
 }
