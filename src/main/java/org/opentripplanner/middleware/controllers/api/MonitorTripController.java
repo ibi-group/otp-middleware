73fa15e03f796ff345760266310f50f6b511958f
Rob Beer <rbeer@ibigroup.com>
2020-06-02 13:10:11 +0100

    Updated auth to use OTP user already in request

--- src/main/java/org/opentripplanner/middleware/controllers/api/MonitorTripController.java
+++ src/main/java/org/opentripplanner/middleware/controllers/api/MonitorTripController.java
@@ -3,9 +3,8 @@ package org.opentripplanner.middleware.controllers.api;
 import com.mongodb.client.model.Filters;
 import org.bson.conversions.Bson;
 import org.eclipse.jetty.http.HttpStatus;
-import org.opentripplanner.middleware.auth.Auth0Utils;
+import org.opentripplanner.middleware.auth.Auth0UserProfile;
 import org.opentripplanner.middleware.models.MonitoredTrip;
-import org.opentripplanner.middleware.models.User;
 import org.opentripplanner.middleware.persistence.Persistence;
 import spark.Request;
 
@@ -17,6 +16,7 @@ import static org.opentripplanner.middleware.utils.JsonUtils.logMessageAndHalt;
  * with Auth0 services using the hooks provided by {@link ApiController}.
  */
 public class MonitorTripController extends ApiController<MonitoredTrip> {
+    private static final long MAXIMUM_ALLOWED_MONITORED_TRIPS = 5;
 
     public MonitorTripController(String apiPrefix) {
         super(apiPrefix, Persistence.monitoredTrip, "secure/monitortrip");
@@ -24,7 +24,8 @@ public class MonitorTripController extends ApiController<MonitoredTrip> {
 
     @Override
     MonitoredTrip preCreateHook(MonitoredTrip monitoredTrip, Request req) {
-        isValidUser(monitoredTrip.userId, req);
+        isValidUser(req);
+        isAuthorized(monitoredTrip.userId, req);
         reachedMaximum(monitoredTrip.userId, req);
 
         return monitoredTrip;
@@ -32,42 +33,50 @@ public class MonitorTripController extends ApiController<MonitoredTrip> {
 
     @Override
     MonitoredTrip preUpdateHook(MonitoredTrip monitoredTrip, MonitoredTrip preExisting, Request req) {
-        isValidUser(monitoredTrip.userId, req);
-
+        isValidUser(req);
+        isAuthorized(monitoredTrip.userId, req);
         return monitoredTrip;
     }
 
     @Override
     boolean preDeleteHook(MonitoredTrip monitoredTrip, Request req) {
-        isValidUser(monitoredTrip.userId, req);
+        isValidUser(req);
+        isAuthorized(monitoredTrip.userId, req);
         return true;
     }
 
     /**
-     * Confirm that the user exists and is authorized
+     * Confirm that the user exists
      */
-    private void isValidUser(String userId, Request request) {
+    private void isValidUser(Request request) {
 
-        User user = Persistence.users.getById(userId);
-        if (user == null) {
+        Auth0UserProfile profile = request.attribute("user");
+        if (profile.otpUser == null) {
             logMessageAndHalt(request, HttpStatus.FORBIDDEN_403, "Unknown user.");
         }
+    }
 
-        Auth0Utils.isAuthorizedUser(user, request);
+    /**
+     * Confirm that the user's actions are on their own monitored trips.
+     */
+    private void isAuthorized(String userId, Request request) {
 
+        Auth0UserProfile profile = request.attribute("user");
+        if (!profile.otpUser.id.equalsIgnoreCase(userId)) {
+            logMessageAndHalt(request, HttpStatus.FORBIDDEN_403, "Unauthorized access");
+        }
     }
 
     /**
      * Confirm that the maximum number of saved monitored trips has not been reached
      */
     private void reachedMaximum(String userId, Request request) {
-        long maximum = 5;
 
         // filter monitored trip on user id to find out how many have already been saved
         Bson filter = Filters.and(eq("userId", userId));
         long count = this.persistence.getCountFiltered(filter);
-        if (count == maximum) {
-            logMessageAndHalt(request, HttpStatus.BAD_REQUEST_400, "Maximum amount of saved monitored trips reached. Maximum = " + maximum);
+        if (count >= MAXIMUM_ALLOWED_MONITORED_TRIPS) {
+            logMessageAndHalt(request, HttpStatus.BAD_REQUEST_400, "Maximum amount of saved monitored trips reached. Maximum = " + MAXIMUM_ALLOWED_MONITORED_TRIPS);
         }
     }
 }
