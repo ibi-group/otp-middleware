4bd67695189e10d87c20b44ef9bc7522d3ed3825
Rob Beer <rbeer@ibigroup.com>
2020-06-09 10:28:42 +0100

    Fixed failing tests

--- src/test/java/org/opentripplanner/middleware/OtpDispatcherHelper.java
+++ src/test/java/org/opentripplanner/middleware/OtpDispatcherHelper.java
@@ -2,6 +2,8 @@ package org.opentripplanner.middleware;
 
 import org.opentripplanner.middleware.otp.OtpDispatcherImpl;
 import org.opentripplanner.middleware.otp.OtpDispatcherResponse;
+import org.opentripplanner.middleware.otp.response.Response;
+import org.opentripplanner.middleware.utils.JsonUtils;
 
 /**
  * Used to provide real plan responses from OTP
@@ -17,6 +19,8 @@ public class OtpDispatcherHelper {
     public static OtpDispatcherResponse getPlanFromOtp() {
         OtpDispatcherImpl otpDispatcher = new OtpDispatcherImpl(OTP_SERVER);
         OtpDispatcherResponse response = otpDispatcher.getPlan("plan?userId=123456&fromPlace=28.54894%2C%20-81.38971%3A%3A28.548944048426772%2C-81.38970606029034&toPlace=28.53989%2C%20-81.37728%3A%3A28.539893820446867%2C-81.37727737426759&date=2020-05-05&time=12%3A04&arriveBy=false&mode=WALK%2CBUS%2CRAIL&showIntermediateStops=true&maxWalkDistance=1207&optimize=QUICK&walkSpeed=1.34&ignoreRealtimeUpdates=true&companies=", OTP_SERVER_PLAN_END_POINT);
+        // convert plan response into concrete POJOs
+        response.response = JsonUtils.getPOJOFromJSON(response.responseBody, Response.class);
         System.out.println("OTP Plan response:" + response.toString());
         return response;
     }
@@ -28,6 +32,8 @@ public class OtpDispatcherHelper {
         OtpDispatcherImpl otpDispatcher = new OtpDispatcherImpl(OTP_SERVER);
         // fromPalce instead of fromPlace to produce error
         OtpDispatcherResponse response = otpDispatcher.getPlan("plan?userId=123456&fromPalce=28.54894%2C%20-81.38971%3A%3A28.548944048426772%2C-81.38970606029034&toPlace=28.53989%2C%20-81.37728%3A%3A28.539893820446867%2C-81.37727737426759&date=2020-05-05&time=12%3A04&arriveBy=false&mode=WALK%2CBUS%2CRAIL&showIntermediateStops=true&maxWalkDistance=1207&optimize=QUICK&walkSpeed=1.34&ignoreRealtimeUpdates=true&companies=", OTP_SERVER_PLAN_END_POINT);
+        // convert plan response into concrete POJOs
+        response.response = JsonUtils.getPOJOFromJSON(response.responseBody, Response.class);
         System.out.println("OTP Plan error response:" + response.toString());
         return response;
     }
