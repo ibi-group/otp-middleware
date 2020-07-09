b553faedd3532088a95eabafccc34036c65001f0
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2020-07-09 14:41:52 -0400

    improvement(ApiUser): Add basic ApiUser info.

--- src/main/java/org/opentripplanner/middleware/models/ApiUser.java
+++ src/main/java/org/opentripplanner/middleware/models/ApiUser.java
@@ -8,5 +8,26 @@ import java.util.List;
  * access otp-middleware's endpoints (as well as the geocoding and OTP endpoints).
  */
 public class ApiUser extends AbstractUser {
+    /** Holds the API keys assigned to the user. */
     public List<String> apiKeyIds = new ArrayList<>();
+
+    /** The name of the application built by this user. */
+    public String appName;
+
+    /** The purpose of the application built by this user. */
+    public String appPurpose;
+
+    /** The URL of the application built by this user. */
+    public String appUrl;
+
+    /** The company or organization that this user belongs to. */
+    public String company;
+
+    // FIXME: Move this member to AbstractUser?
+    /** Whether the user has consented to terms of use. */
+    public boolean hasConsentedToTerms;
+
+    // FIXME: Move this member to AbstractUser?
+    /** The name of this user */
+    public String name;
 }
