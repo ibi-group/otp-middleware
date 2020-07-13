c7a81df65b367a93eeb829da22721b00acff8b90
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2020-07-13 13:37:06 -0400

    docs(Bugsnag): Add API docs for Bugsnag

--- src/main/java/org/opentripplanner/middleware/bugsnag/EventSummary.java
+++ src/main/java/org/opentripplanner/middleware/bugsnag/EventSummary.java
@@ -8,6 +8,37 @@ import java.util.Date;
 import java.util.List;
 
 /**
+ * @schema EventSummary
+ * description: "Summary information for events logged in Bugsnag. For additional information: https://bugsnagapiv2.docs.apiary.io/#reference/organizations/event-data-requests/create-an-event-data-requestData structure for log response. The data is based on https://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/AmazonWebServiceResult.html, with items having the fields outlined in https://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/apigateway/model/GetUsageResult.html."
+ * type: object
+ * properties:
+ *   errorId:
+ *     type: string
+ *     description: The error which this event is associated with.
+ *   exceptions:
+ *     type: array
+ *     items:
+ *       type: object # TODO - to implement.
+ *     description: A list of EventException classes and messages associated with this event.
+ *   received:
+ *     type: string
+ *     # TODO - format:
+ *     description: The date/time Bugsnag received the event.
+ *   projectId:
+ *     type: string
+ *     description: The project which this event is associated with.
+ *   projectName:
+ *     type: string
+ *     description: The associated project name.
+ *   releaseState:
+ *     type: string
+ *     description: Associated environment e.g. Test, Dev, Production.
+ *   htmlUrl:
+ *     type: string
+ *     description: The dashboard URL for the project.
+ */
+
+/**
  * Event summary information provided to calling services. Response is based on static project and dynamic event
  * information. Additional information relating to this can be found here:
  * https://bugsnagapiv2.docs.apiary.io/#reference/organizations/event-data-requests/create-an-event-data-request
--- src/main/java/org/opentripplanner/middleware/controllers/api/BugsnagController.java
+++ src/main/java/org/opentripplanner/middleware/controllers/api/BugsnagController.java
@@ -23,6 +23,65 @@ public class BugsnagController {
     private static TypedPersistence<BugsnagProject> bugsnagProjects = Persistence.bugsnagProjects;
 
     /**
+     * @api [get] /api/admin/bugsnag/eventsummary
+     * tags:
+     * - "api/admin/bugsnag/eventsummary"
+     * description: "Gets a list of recent Bugsnag events."
+     * parameters:
+     * - $ref: '#/components/parameters/AWSAuthHeaderRequired'
+     * security:
+     * - ApiKey: [] # Required for AWS integration.
+     *   Auth0Bearer: []
+     * responses:
+     *   200:
+     *     description: "successful operation"
+     *     content:
+     *       application/json:
+     *         schema:
+     *           type: array
+     *           items:
+     *             $ref: "#/components/schemas/EventSummary"
+     *     headers:
+     *       Access-Control-Allow-Origin:
+     *         schema:
+     *           type: "string"
+     * x-amazon-apigateway-integration:
+     *   uri: "http://54.147.40.127/api/admin/bugsnag/eventsummary"
+     *   responses:
+     *     default:
+     *       statusCode: "200"
+     *       responseParameters:
+     *         method.response.header.Access-Control-Allow-Origin: "'*'"
+     *   requestParameters:
+     *     integration.request.header.Authorization: "method.request.header.Authorization"
+     *   passthroughBehavior: "when_no_match"
+     *   httpMethod: "GET"
+     *   type: "http"
+     *
+     */
+
+    /**
+     * @api [options] /api/admin/bugsnag/eventsummary
+     * responses:
+     *   200:
+     *     $ref: "#/components/responses/AllowCORS"
+     * tags:
+     *  - "api/admin/bugsnag/eventsummary"
+     * x-amazon-apigateway-integration:
+     *   uri: "http://54.147.40.127/api/admin/bugsnag/eventsummary"
+     *   responses:
+     *     default:
+     *       statusCode: 200
+     *       responseParameters:
+     *         method.response.header.Access-Control-Allow-Methods: "'GET,OPTIONS'"
+     *         method.response.header.Access-Control-Allow-Headers: "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+     *         method.response.header.Access-Control-Allow-Origin: "'*'"
+     *   passthroughBehavior: when_no_match
+     *   httpMethod: OPTIONS
+     *   type: http
+     */
+
+    /**
      * Get all Bugsnag events from Mongo and replace the project id with the project name and return
      */
     public static String getEventSummary(Request request, Response response) {
