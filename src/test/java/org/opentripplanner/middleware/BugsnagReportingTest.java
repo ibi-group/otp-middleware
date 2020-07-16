e8d154ed7179d52530e8aadc33537b2da553bce4
Rob Beer <rbeer@ibigroup.com>
2020-07-16 14:21:42 +0100

    improvement(Unit test update): Updated to include assertions!

--- src/test/java/org/opentripplanner/middleware/BugsnagReportingTest.java
+++ src/test/java/org/opentripplanner/middleware/BugsnagReportingTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.middleware.bugsnag.BugsnagReporter;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * Tests for Bugsnag reporting. All are set to @Disabled because a BUGSNAG_PROJECT_NOTIFIER_API_KEY is required in the
  * test\env.yml file which gets committed. If these tests are required to be run, the README explains how to get a
@@ -13,11 +15,11 @@ public class BugsnagReportingTest extends OtpMiddlewareTest {
 
     @Test @Disabled
     public void createErrorReportWithExceptionAndMessage() {
-        BugsnagReporter.reportErrorToBugsnag(new Exception("Unit test exception 1"), "Unit test 1");
+        assertTrue(BugsnagReporter.reportErrorToBugsnag(new Exception("Unit test exception 1"), "Unit test 1"));
     }
 
     @Test @Disabled
     public void createErrorReportWithException() {
-        BugsnagReporter.reportErrorToBugsnag(new Exception("Unit test exception 2"), null);
+        assertTrue(BugsnagReporter.reportErrorToBugsnag(new Exception("Unit test exception 2"), null));
     }
 }
