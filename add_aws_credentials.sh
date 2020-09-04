ccca111000b6dda10cf8c58e8428139018dc1301
Landon Reed <landontreed@gmail.com>
2020-09-04 10:45:32 -0400

    ci(travis): update credentials script comments

--- add_aws_credentials.sh
+++ add_aws_credentials.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # This script will create the AWS credentials file if it does not exist.
 # It is only meant to be run on Travis CI (to create the proper
-# environment for E2E tests.
+# environment for E2E tests).
 mkdir -p ~/.aws
 
 # If credentials do not exist, create file setting values to
 # environment variables (which must be defined in Travis CI).
+# This should avoid any accidental overwrite on your local dev machine :)
 if [ ! -f ~/.aws/credentials ]; then
 cat > ~/.aws/credentials << EOL
 [default]
