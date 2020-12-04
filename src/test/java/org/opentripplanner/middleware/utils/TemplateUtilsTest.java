2c91957c0f64c62735143be79408dc9bfa3d01a6
Evan Siroky <evan.siroky@yahoo.com>
2020-12-03 16:39:35 -0800

    test(templates): add unique names for template tests

--- src/test/java/org/opentripplanner/middleware/utils/TemplateUtilsTest.java
+++ src/test/java/org/opentripplanner/middleware/utils/TemplateUtilsTest.java
@@ -7,8 +7,10 @@ import org.opentripplanner.middleware.OtpMiddlewareTest;
 import org.opentripplanner.middleware.TestUtils;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static com.zenika.snapshotmatcher.SnapshotMatcher.matchesSnapshot;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,12 +23,18 @@ public class TemplateUtilsTest extends OtpMiddlewareTest {
     }
 
     /**
-     * A parameterized test that checks whether various templates render in a way that matches a snapshot.
+     * A parameterized test that checks whether various templates render in a way that matches a snapshot. The name of
+     * the test case is guaranteed to be unique due to a check in the {@link TemplateRenderingTestCase} constructor. The
+     * test case name is then used to generate uniquely-named snapshots so that individual test cases can be properly be
+     * ran.
      */
     @ParameterizedTest
     @MethodSource("createTemplateRenderingTestCases")
     public void canRenderTemplates(TemplateRenderingTestCase testCase) throws Exception {
-        assertThat(TemplateUtils.renderTemplate(testCase.templatePath, testCase.data), matchesSnapshot());
+        assertThat(
+            TemplateUtils.renderTemplate(testCase.templatePath, testCase.data),
+            matchesSnapshot(testCase.testCaseName)
+        );
     }
 
     /**
@@ -39,30 +47,54 @@ public class TemplateUtilsTest extends OtpMiddlewareTest {
 
         // Event Errors (this is a template for an email that is sent in BugsnagEventHandlingJob#sendEmailForEvents)
         Map<String, String> errorEventsData = Map.of("subject", "test subject");
-        testCases.add(new TemplateRenderingTestCase(errorEventsData, "EventErrorsText.ftl"));
-        testCases.add(new TemplateRenderingTestCase(errorEventsData, "EventErrorsHtml.ftl"));
+        testCases.add(new TemplateRenderingTestCase(
+            errorEventsData, "EventErrorsText.ftl", "Event Errors Text Email"
+        ));
+        testCases.add(new TemplateRenderingTestCase(
+            errorEventsData, "EventErrorsHtml.ftl", "Event Errors Html Email"
+        ));
+
         // Trip Monitor Notifications tests (for CheckMonitoredTrip#sendNotifications).
         Map<String, Object> notificationsData = Map.of(
             "tripId", "18f642d5-f7a8-475a-9469-800129e6c0b3",
             "notifications", List.of("Test notification.", "Another notification.")
         );
-        testCases.add(new TemplateRenderingTestCase(notificationsData, "MonitoredTripSms.ftl"));
-        testCases.add(new TemplateRenderingTestCase(notificationsData, "MonitoredTripText.ftl"));
-        testCases.add(new TemplateRenderingTestCase(notificationsData, "MonitoredTripHtml.ftl"));
+        testCases.add(new TemplateRenderingTestCase(
+            notificationsData, "MonitoredTripSms.ftl", "Monitored Trip SMS"
+        ));
+        testCases.add(new TemplateRenderingTestCase(
+            notificationsData, "MonitoredTripText.ftl", "Monitored Trip Text Email"
+        ));
+        testCases.add(new TemplateRenderingTestCase(
+            notificationsData, "MonitoredTripHtml.ftl", "Monitored Trip HTML Email"
+        ));
         return testCases;
     }
 
     private static class TemplateRenderingTestCase {
+        private static final Set<String> testCaseNames = new HashSet<>();
         public final Object data;
         public final String templatePath;
+        public final String testCaseName;
 
-        private TemplateRenderingTestCase(Object data, String templatePath) {
+        private TemplateRenderingTestCase(Object data, String templatePath, String testCaseName) {
+            // enforce unique test case names so that snapshotting works properly
+            if (testCaseNames.contains(testCaseName)) {
+                throw new IllegalArgumentException(
+                    String.format(
+                        "A test case with the name %s already exists! Test case names must be unique.",
+                        testCaseName
+                    )
+                );
+            }
+            testCaseNames.add(testCaseName);
             this.data = data;
             this.templatePath = templatePath;
+            this.testCaseName = testCaseName;
         }
 
         @Override public String toString() {
-            return templatePath;
+            return testCaseName;
         }
     }
 }
