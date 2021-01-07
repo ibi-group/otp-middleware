28ddfa9eed23362727c3f4fff34fe87073f11f34
Evan Siroky <evan.siroky@yahoo.com>
2021-01-06 20:17:24 -0500

    ci: try to get tests to pass on GitHub Actions 7/n

--- src/main/java/org/opentripplanner/middleware/utils/ConfigUtils.java
+++ src/main/java/org/opentripplanner/middleware/utils/ConfigUtils.java
@@ -29,6 +29,13 @@ public class ConfigUtils {
 
     private static JsonNode envConfig;
 
+    static {
+        LOG.info("Environment variables:");
+        for (String key : System.getenv().keySet()) {
+            LOG.info(key);
+        }
+    }
+
     /**
      * Returns true only if an environment variable exists and is set to "true".
      */
