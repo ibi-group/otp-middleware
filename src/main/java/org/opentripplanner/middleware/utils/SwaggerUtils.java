873024b1c001192cba64ab09b135e39a32f22329
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-03-22 13:39:58 -0400

    refactor(PublicApiDocGenerator): Refactor to make tests pass.

--- src/main/java/org/opentripplanner/middleware/utils/SwaggerUtils.java
+++ src/main/java/org/opentripplanner/middleware/utils/SwaggerUtils.java
@@ -5,6 +5,7 @@ import io.github.manusant.ss.model.Response;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class SwaggerUtils {
     private SwaggerUtils() {}
@@ -29,19 +30,19 @@ public class SwaggerUtils {
     public static Map<String, Response> createStandardResponses() {
         Map<String, Object> emptyExamples = new LinkedHashMap<>();
 
-        Map<String, Response> responses = Map.of(
-            "200", new Response().description("Successful operation"),
-            "400", new Response().description("The request was not formed properly " +
-                    "(e.g., some required parameters may be missing). " +
-                    "See the details of the returned response to determine the exact issue."),
-            "401", new Response().description("The server was not able to authenticate the request. " +
-                    "This can happen if authentication headers are missing or malformed, " +
-                    "or the authentication server cannot be reached."),
-            "403", new Response().description("The requesting user is not allowed to perform the request."),
-            "404", new Response().description("The requested item was not found."),
-            "500", new Response().description("An error occurred while performing the request. " +
-                    "Contact an API administrator for more information.")
-        );
+        // Using a TreeMap so that entries are output sorted, and unit tests results are consistent.
+        Map<String, Response> responses = new TreeMap<>();
+        responses.put("200", new Response().description("Successful operation"));
+        responses.put("400", new Response().description("The request was not formed properly " +
+                "(e.g., some required parameters may be missing). " +
+                "See the details of the returned response to determine the exact issue."));
+        responses.put("401", new Response().description("The server was not able to authenticate the request. " +
+                "This can happen if authentication headers are missing or malformed, " +
+                "or the authentication server cannot be reached."));
+        responses.put("403", new Response().description("The requesting user is not allowed to perform the request."));
+        responses.put("404", new Response().description("The requested item was not found."));
+        responses.put("500", new Response().description("An error occurred while performing the request. " +
+                "Contact an API administrator for more information."));
 
         // All responses get no examples, so that no extra docs are generated for error cases.
         // (spark-swagger will still generate a basic example for the 200 response.)
