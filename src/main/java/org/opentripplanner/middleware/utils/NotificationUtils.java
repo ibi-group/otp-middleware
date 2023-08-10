eb2d20742c61a4deed6b16b556318a49ed1433fa
Jym Dyer <jym.dyer@ibigroup.com>
2023-08-10 14:00:16 -0700

    Add sendPush() method for push notifications.

--- src/main/java/org/opentripplanner/middleware/utils/NotificationUtils.java
+++ src/main/java/org/opentripplanner/middleware/utils/NotificationUtils.java
@@ -9,18 +9,24 @@ import com.twilio.rest.verify.v2.service.VerificationCheck;
 import com.twilio.rest.verify.v2.service.VerificationCreator;
 import com.twilio.type.PhoneNumber;
 import freemarker.template.TemplateException;
+import org.eclipse.jetty.http.HttpMethod;
 import org.opentripplanner.middleware.bugsnag.BugsnagReporter;
 import org.opentripplanner.middleware.models.AdminUser;
 import org.opentripplanner.middleware.models.OtpUser;
+import org.opentripplanner.middleware.utils.HttpResponseValues;
+import org.opentripplanner.middleware.utils.HttpUtils;
+import org.opentripplanner.middleware.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
 
 import static org.opentripplanner.middleware.utils.ConfigUtils.getConfigPropertyAsText;
 
 /**
- * This class contains utils for sending SMS and email notifications.
+ * This class contains utils for sending SMS, email, and push notifications.
  *
  * TODO: It may be better to initialize all of these notification clients in a static block? This may not really be
  *  necessary though -- needs some more research.
@@ -36,6 +42,52 @@ public class NotificationUtils {
     private static final String SPARKPOST_KEY = getConfigPropertyAsText("SPARKPOST_KEY");
     private static final String FROM_EMAIL = getConfigPropertyAsText("NOTIFICATION_FROM_EMAIL");
     public static final String OTP_ADMIN_DASHBOARD_FROM_EMAIL = getConfigPropertyAsText("OTP_ADMIN_DASHBOARD_FROM_EMAIL");
+    private static final String PUSH_API_KEY = getConfigPropertyAsText("PUSH_API_KEY");
+    private static final String PUSH_API_URL = "https://demo.ibigroupmobile.com/ext_api";
+
+    /**
+     * @param otpUser  target user
+     * @param textTemplate  template to use for email in text format
+     * @param templateData          template data
+     */
+    public static String sendPush(OtpUser otpUser, String textTemplate, Object templateData) {
+        try {
+            String body = TemplateUtils.renderTemplate(textTemplate, templateData);
+            return sendPush(otpUser.email, body);
+        } catch (TemplateException | IOException e) {
+            // This catch indicates there was an error rendering the template. Note: TemplateUtils#renderTemplate
+            // handles Bugsnag reporting/error logging, so that is not needed here.
+            return null;
+        }
+    }
+
+    /**
+     * Send a push notification message to the provided user
+     * @param toUser    user account ID (email address)
+     * @param body      message body
+     * @return          "OK" if message was successful (null otherwise)
+     */
+    public static String sendPush(String toUser, String body) {
+        try {
+            var jsonBody = "{\"user\":\"" + toUser + "\",\"message\":\"" + body + "\"}";
+            Map<String, String> headers = Map.of("Accept", "application/json");
+            var httpResponse = HttpUtils.httpRequestRawResponse(
+                URI.create(PUSH_API_URL + "/devices/get?api_key=" + PUSH_API_KEY + "&user=" + toUser),
+                1000,
+                HttpMethod.POST,
+                headers,
+                jsonBody
+            );
+            if (httpResponse.status == 200) {
+                return "OK";
+            } else {
+                LOG.error("Error {} while trying to initiate push notification", httpResponse.status);
+            }
+        } catch (Exception e) {
+            LOG.error("Could not initiate push notification", e);
+        }
+        return null;
+    }
 
     /**
      * Send templated SMS to {@link OtpUser}'s verified phone number.
