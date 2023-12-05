6ac00ba46d624e578d309545bed3fb0456b6ca59
Jym Dyer <jym.dyer@ibigroup.com>
2023-12-05 14:34:10 -0800

    OTP-888 - Parameterized tests.

--- src/test/java/org/opentripplanner/middleware/models/MobilityProfileTest.java
+++ src/test/java/org/opentripplanner/middleware/models/MobilityProfileTest.java
@@ -1,11 +1,13 @@
 package org.opentripplanner.middleware.models;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * This class contains tests of selected scenarios in {@link MobilityProfile}.
@@ -16,47 +18,43 @@ public class MobilityProfileTest {
     // implemented in the MobilityPorfile#updateMobilityMode() method.  Changes
     // to that document must be reflected in that method and in these tests.
 
-    @Test
-    public void testModesKnown() {
-        var prof = new MobilityProfile();
-        prof.mobilityDevices = Set.of("service animal", "crutches");
-        prof.updateMobilityMode();
-        Assertions.assertEquals(prof.mobilityMode, "Device");
-        prof.mobilityDevices = Set.of("service animal", "electric wheelchair");
-        prof.updateMobilityMode();
-        Assertions.assertEquals(prof.mobilityMode, "WChairE");
-        prof.mobilityDevices = Set.of("service animal", "electric wheelchair", "white cane");
-        prof.updateMobilityMode();
-        Assertions.assertEquals(prof.mobilityMode, "WChairE-Blind");
-        prof.mobilityDevices = Set.of("manual wheelchair", "electric wheelchair", "white cane");
-        prof.updateMobilityMode();
-        Assertions.assertEquals(prof.mobilityMode, "WChairM-Blind");
+    private static Stream<Arguments> provideModes() {
+        return Stream.of(
+            Arguments.of(Set.of("service animal", "crutches"), "Device"),
+            Arguments.of(Set.of("service animal", "electric wheelchair"), "WChairE"),
+            Arguments.of(Set.of("service animal", "electric wheelchair", "white cane"), "WChairE-Blind"),
+            Arguments.of(Set.of("manual wheelchair", "electric wheelchair", "white cane"), "WChairM-Blind"),
+            Arguments.of(Collections.EMPTY_SET, "None"),
+            Arguments.of(Set.of("cardboard transmogrifier"), "None"), // Unknown/invalid device
+            Arguments.of(Set.of("cane", "none", "service animal"), "None") // Devices include "none" poison pill
+        );
     }
 
-    @Test
-    public void testModesNone() {
+    @ParameterizedTest
+    @MethodSource("provideModes")
+    public void testModes(Set<String> devices, String mode) {
         var prof = new MobilityProfile();
-        prof.mobilityDevices = Collections.EMPTY_LIST;
+        prof.mobilityDevices = devices;
         prof.updateMobilityMode();
-        Assertions.assertEquals(prof.mobilityMode, "None");
-        prof.mobilityDevices = Set.of("cardboard transmogrifier"); // Unknown/invalid device
-        prof.updateMobilityMode();
-        Assertions.assertEquals(prof.mobilityMode, "None");
-        prof.mobilityDevices = Set.of("cane", "none", "service animal"); // Devices with "none" poison pill
-        prof.updateMobilityMode();
-        Assertions.assertEquals(prof.mobilityMode, "None");
+        Assertions.assertEquals(mode, prof.mobilityMode);
     }
 
-    @Test
-    public void testModesVision() {
+    private static Stream<Arguments> provideModesVision() {
+        return Stream.of(
+            Arguments.of(MobilityProfile.VisionLimitation.LOW_VISION, // Overrides "white cane" default
+                    Set.of("service animal", "electric wheelchair", "white cane"), "WChairE-LowVision"),
+            Arguments.of(MobilityProfile.VisionLimitation.LEGALLY_BLIND,
+                    Set.of("manual wheelchair", "stroller"), "WChairM-Blind")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideModesVision")
+    public void testModesVision(MobilityProfile.VisionLimitation limitation, Set<String> devices, String mode) {
         var prof = new MobilityProfile();
-        prof.mobilityDevices = Set.of("service animal", "electric wheelchair", "white cane");
-        prof.visionLimitation = MobilityProfile.VisionLimitation.LOW_VISION; // Overrides "white cane" effect
+        prof.mobilityDevices = devices;
+        prof.visionLimitation = limitation;
         prof.updateMobilityMode();
-        Assertions.assertEquals(prof.mobilityMode, "WChairE-LowVision");
-        prof.mobilityDevices = Set.of("manual wheelchair", "stroller");
-        prof.visionLimitation = MobilityProfile.VisionLimitation.LEGALLY_BLIND;
-        prof.updateMobilityMode();
-        Assertions.assertEquals(prof.mobilityMode, "WChairM-Blind");
+        Assertions.assertEquals(mode, prof.mobilityMode);
     }
 }
