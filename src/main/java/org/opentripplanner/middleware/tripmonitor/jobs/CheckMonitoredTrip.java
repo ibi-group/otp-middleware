fb8c783196264b0ca407e9164287debedd5284b4
Adriana Cerić <62163307+AdrianaCeric@users.noreply.github.com>
2023-12-20 10:33:01 -0500

    fix: clean up sendNotifications function

--- src/main/java/org/opentripplanner/middleware/tripmonitor/jobs/CheckMonitoredTrip.java
+++ src/main/java/org/opentripplanner/middleware/tripmonitor/jobs/CheckMonitoredTrip.java
@@ -428,14 +428,7 @@ public class CheckMonitoredTrip implements Runnable {
         Map<String, Object> templateData = Map.of(
             "tripId", trip.id,
             "tripName", trip.tripName,
-            "notifications", new ArrayList<>(notifications).stream()
-                // Make certain notifications, such as the initial reminder one, appear on top.
-                .sorted(Comparator.comparingInt(TripMonitorNotification::sortOrder))
-                .map(notification -> Map.of(
-                        "type", notification.type.toString(),
-                        "body", notification.body
-                ))
-                .collect(Collectors.toList())
+            "notifications", new ArrayList<>(notifications)
         );
         // FIXME: Change log level
         LOG.info("Sending notification to user {}", trip.userId);
