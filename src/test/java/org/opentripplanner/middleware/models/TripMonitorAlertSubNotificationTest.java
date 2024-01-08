ffbce7e22c061a734dea552aa98ff0b07e2ed341
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-01-08 17:39:51 -0500

    test(TripMonitorAlertNotification): Add tests for when to create alert notif and test plain text con

--- src/test/java/org/opentripplanner/middleware/models/TripMonitorAlertSubNotificationTest.java
+++ src/test/java/org/opentripplanner/middleware/models/TripMonitorAlertSubNotificationTest.java
@@ -2,16 +2,16 @@ package org.opentripplanner.middleware.models;
 
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.middleware.otp.response.LocalizedAlert;
-import org.opentripplanner.middleware.tripmonitor.jobs.NotificationType;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 class TripMonitorAlertSubNotificationTest {
+
+    public static final String NEW_ALERT_NOTIFICATION_TEXT = String.format("New alerts found:%n%n- Alert description");
+
     @Test
     void canProduceNewAlertPlainText() {
         Set<LocalizedAlert> newAlerts = new HashSet<>();
@@ -21,7 +21,7 @@ class TripMonitorAlertSubNotificationTest {
             "New alerts found:",
             newAlerts
         );
-        assertEquals(String.format("New alerts found:%n%n- Alert description"), newAlertSubNotification.toString());
+        assertEquals(NEW_ALERT_NOTIFICATION_TEXT, newAlertSubNotification.toString());
     }
 
     @Test
@@ -33,7 +33,10 @@ class TripMonitorAlertSubNotificationTest {
             "Resolved alerts:",
             newAlerts
         );
-        assertEquals(String.format("Resolved alerts:%n%n- (RESOLVED) Alert description"), resolvedAlertSubNotification.toString(true));
+        assertEquals(
+            String.format("Resolved alerts:%n%n- (RESOLVED) Alert description"),
+            resolvedAlertSubNotification.toString(true)
+        );
     }
 
     private static LocalizedAlert createAlert() {
