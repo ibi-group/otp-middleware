49f9eab6e8a0da0be435ede862a4eb1811cce313
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-03-20 15:34:29 -0400

    improvement(CheckMonitoredTripBasicTest): Add tests with recurring trips.

--- src/test/java/org/opentripplanner/middleware/tripmonitor/jobs/CheckMonitoredTripBasicTest.java
+++ src/test/java/org/opentripplanner/middleware/tripmonitor/jobs/CheckMonitoredTripBasicTest.java
@@ -6,8 +6,11 @@ import org.opentripplanner.middleware.models.MonitoredTrip;
 import org.opentripplanner.middleware.otp.response.Itinerary;
 import org.opentripplanner.middleware.utils.DateTimeUtils;
 
+import java.time.DayOfWeek;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.stream.Stream;
@@ -18,6 +21,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * This class contains tests for {@link CheckMonitoredTrip} that don't require database or OTP queries.
  */
 class CheckMonitoredTripBasicTest {
+    private static final int ONE_DAY_IN_SECONDS = 3600 * 24;
+
     @ParameterizedTest
     @MethodSource("createSkipMonitoringCases")
     void testSkipMonitoredTripCheck(SkipMonitoringTestArgs args) throws Exception {
@@ -31,6 +36,10 @@ class CheckMonitoredTripBasicTest {
         trip.tripTime = DateTimeUtils.makeOtpZonedDateTime(start).format(DateTimeFormatter.ISO_LOCAL_TIME);
         trip.leadTimeInMinutes = 30;
 
+        if (args.isRecurring) {
+            setMonitoredDaysForTest(trip);
+        }
+
         assertEquals(
             args.result,
             new CheckMonitoredTrip(trip).shouldSkipMonitoredTripCheck(false),
@@ -42,7 +51,50 @@ class CheckMonitoredTripBasicTest {
         return Stream.of(
             new SkipMonitoringTestArgs(-300, -5, false, true, "Should skip monitoring one-time trip in the past"),
             new SkipMonitoringTestArgs(300, 500, false, false, "Should not skip monitoring upcoming one-time trip"),
-            new SkipMonitoringTestArgs(3600, 3900, false, true, "Should skip monitoring one-time trip in the future")
+            new SkipMonitoringTestArgs(3600, 3900, false, true, "Should skip monitoring one-time trip in the future"),
+            new SkipMonitoringTestArgs(-300, -5, true, true, "Should skip monitoring recurring trip that just concluded for today"),
+            new SkipMonitoringTestArgs(300, 500, false, false, "Should not skip monitoring upcoming recurring trip"),
+            new SkipMonitoringTestArgs(360 - ONE_DAY_IN_SECONDS, 500 - ONE_DAY_IN_SECONDS, true, false, "Should not skip recurring trip monitored on the following day")
         );
+    }
+
+    /** Add the day-of-week of the itinerary start time as the recurring day, and the next day too. */
+    private static void setMonitoredDaysForTest(MonitoredTrip trip) {
+        DayOfWeek dayOfWeek = DayOfWeek.of(LocalDate.ofInstant(
+            trip.itinerary.startTime.toInstant(),
+            DateTimeUtils.getOtpZoneId()).get(ChronoField.DAY_OF_WEEK
+        ));
+        switch (dayOfWeek) {
+            case MONDAY:
+                trip.monday = true;
+                trip.tuesday = true;
+                break;
+            case TUESDAY:
+                trip.tuesday = true;
+                trip.wednesday = true;
+                break;
+            case WEDNESDAY:
+                trip.wednesday = true;
+                trip.thursday = true;
+                break;
+            case THURSDAY:
+                trip.thursday = true;
+                trip.friday = true;
+                break;
+            case FRIDAY:
+                trip.friday = true;
+                trip.saturday = true;
+                break;
+            case SATURDAY:
+                trip.saturday = true;
+                trip.sunday = true;
+                break;
+            case SUNDAY:
+                trip.sunday = true;
+                trip.monday = true;
+                break;
+            default:
+                break;
+        }
     }
 }
