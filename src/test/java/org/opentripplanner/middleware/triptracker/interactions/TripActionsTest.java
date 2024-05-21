de8adce3f135b0e29766e2a09b0925cc0e7dc34b
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-05-21 18:10:46 -0400

    test(TripActions): Add tests for handling leg steps.

--- src/test/java/org/opentripplanner/middleware/triptracker/interactions/TripActionsTest.java
+++ src/test/java/org/opentripplanner/middleware/triptracker/interactions/TripActionsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.opentripplanner.middleware.otp.response.Step;
 import org.opentripplanner.middleware.triptracker.Segment;
 import org.opentripplanner.middleware.utils.Coordinates;
 
@@ -14,13 +15,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class TripActionsTest {
+    public static final Coordinates SEGMENT1_START = new Coordinates(33.95684, -83.97971);
+    public static final Coordinates SEGMENT1_END = new Coordinates(33.95653, -83.97973);
+    public static final Coordinates SOME_SEGMENT_END = new Coordinates(33.95573, -83.97991);
+    public static final Coordinates SOME_SEGMENT_START = new Coordinates(33.95444, -83.98013);
+
     private final TripActions tripActions = new TripActions(List.of(
         new SegmentAction(
             "segment1",
-            new Segment(
-                new Coordinates(33.95684, -83.97971),
-                new Coordinates(33.95653, -83.97973)
-            ),
+            new Segment(SEGMENT1_START, SEGMENT1_END),
             TrivialTripAction.class.getName()
         ),
         new SegmentAction(
@@ -46,26 +49,65 @@ class TripActionsTest {
 
         assertNull(TrivialTripAction.getLastSegmentId());
         tripActions.handleSegmentAction(segment, null);
-        assertEquals(expectedActionId, TrivialTripAction.getLastSegmentId());
+        assertEquals(expectedActionId, TrivialTripAction.getLastSegmentId(), message);
     }
 
     static Stream<Arguments> createMatchSegmentCases() {
         return Stream.of(
             Arguments.of(
-                new Segment(
-                    new Coordinates(33.95444, -83.98013),
-                    new Coordinates(33.95573, -83.97991)
-                ),
+                new Segment(SOME_SEGMENT_START, SOME_SEGMENT_END),
                 null,
                 "Segment not near a trip action should not match configured segments"
             ),
             Arguments.of(
-                new Segment(
-                    new Coordinates(33.95684, -83.97971),
-                    new Coordinates(33.95653, -83.97973)
-                ),
+                new Segment(SEGMENT1_START, SEGMENT1_END),
                 "segment1",
-                "Segment with coords about the same as a trip action should match"
+                "Segment with coordinates about the same as a trip action should match"
+            )
+        );
+    }
+
+    private Step createStep(Coordinates coords) {
+        Step step = new Step();
+        step.lat = coords.lat;
+        step.lon = coords.lon;
+        return step;
+    }
+
+    @ParameterizedTest
+    @MethodSource("createMatchSegmentFromLegStepsCases")
+    void canMatchSegmentFromLegSteps(int stepIndex, String expectedActionId, String message) {
+        Step stepBeforeSegment1 = createStep(SOME_SEGMENT_END);
+        Step stepOnSegment1 = createStep(SEGMENT1_START);
+        Step stepAfterSegment1 = createStep(SEGMENT1_END);
+
+        List<Step> steps = List.of(
+            stepBeforeSegment1,
+            stepOnSegment1,
+            stepAfterSegment1
+        );
+
+        assertNull(TrivialTripAction.getLastSegmentId());
+        tripActions.handleSegmentAction(steps.get(stepIndex), steps, null);
+        assertEquals(expectedActionId, TrivialTripAction.getLastSegmentId(), message);
+    }
+
+    static Stream<Arguments> createMatchSegmentFromLegStepsCases() {
+        return Stream.of(
+            Arguments.of(
+                0,
+                null,
+                "Leg step not near a trip action should not match configured segments"
+            ),
+            Arguments.of(
+                1,
+                "segment1",
+                "Leg step with coordinates about the same as a trip action should match"
+            ),
+            Arguments.of(
+                2,
+                null,
+                "Leg step not near a trip action should not match configured segments"
             )
         );
     }
