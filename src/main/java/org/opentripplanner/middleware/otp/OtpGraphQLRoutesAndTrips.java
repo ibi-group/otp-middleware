243160f559c619422c8ae5546fd6917f6cf7c55f
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-07-19 11:27:15 -0400

    refactor(OtpGraphQLVariables): Implement Cloneable.

--- src/main/java/org/opentripplanner/middleware/otp/OtpGraphQLRoutesAndTrips.java
+++ src/main/java/org/opentripplanner/middleware/otp/OtpGraphQLRoutesAndTrips.java
@@ -1,7 +1,15 @@
 package org.opentripplanner.middleware.otp;
 
 /** OTP GraphQL data structure for preferred/unpreferred/banned routes and trips */
-public class OtpGraphQLRoutesAndTrips {
+public class OtpGraphQLRoutesAndTrips implements Cloneable {
     public String routes;
     public String trips;
+
+    @Override
+    public OtpGraphQLRoutesAndTrips clone() {
+        OtpGraphQLRoutesAndTrips clone = new OtpGraphQLRoutesAndTrips();
+        clone.routes = routes;
+        clone.trips = trips;
+        return clone;
+    }
 }
--- src/main/java/org/opentripplanner/middleware/otp/OtpGraphQLVariables.java
+++ src/main/java/org/opentripplanner/middleware/otp/OtpGraphQLVariables.java
@@ -1,9 +1,14 @@
 package org.opentripplanner.middleware.otp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import spark.Request;
+
 import java.util.List;
 
+import static org.opentripplanner.middleware.utils.JsonUtils.getPOJOFromJSON;
+
 /** OTP 'plan' query variables */
-public class OtpGraphQLVariables {
+public class OtpGraphQLVariables implements Cloneable {
     public boolean arriveBy;
     public OtpGraphQLRoutesAndTrips banned;
     public float bikeReluctance;
@@ -19,4 +24,29 @@ public class OtpGraphQLVariables {
     public float walkReluctance;
     public float walkSpeed;
     public boolean wheelchair;
+
+    public static OtpGraphQLVariables fromRequest(Request request) throws JsonProcessingException {
+        return getPOJOFromJSON(request.body(), OtpGraphQLQuery.class).variables;
+    }
+
+    @Override
+    public OtpGraphQLVariables clone() {
+        OtpGraphQLVariables clone = new OtpGraphQLVariables();
+        clone.arriveBy = arriveBy;
+        clone.banned = banned;
+        clone.bikeReluctance = bikeReluctance;
+        clone.carReluctance = carReluctance;
+        clone.date = date;
+        clone.fromPlace = fromPlace;
+        clone.modes = List.copyOf(modes);
+        clone.numItineraries = numItineraries;
+        clone.preferred = preferred.clone();
+        clone.time = time;
+        clone.toPlace = toPlace;
+        clone.unpreferred = unpreferred.clone();
+        clone.walkReluctance = walkReluctance;
+        clone.walkSpeed = walkSpeed;
+        clone.wheelchair = wheelchair;
+        return clone;
+    }
 }
