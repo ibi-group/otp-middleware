8e94568e161dcefb4af858e53d3af6f6ff49aee2
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-08-02 17:04:00 -0400

    fix(OtpRequestProcessor): Use batchId sent by UI for grouping OTP plan requests, instead of creating one.

--- src/main/java/org/opentripplanner/middleware/otp/graphql/Query.java
+++ src/main/java/org/opentripplanner/middleware/otp/graphql/Query.java
@@ -1,7 +1,17 @@
 package org.opentripplanner.middleware.otp.graphql;
 
-/** Wraps an OTP GraphQL query data. Field names below are defined by OTP. */
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ * Wraps an OTP GraphQL query data.
+ * Field names below are defined by OTP, except for batchId which helps group OTP plan requests for a particular search.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Query {
+    public String batchId;
+
     public String query;
 
     public QueryVariables variables;
