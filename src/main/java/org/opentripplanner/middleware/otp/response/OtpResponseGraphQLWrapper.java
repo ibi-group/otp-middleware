8c5d13c5371423a490e8b3334ae76004d4bb9035
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-08-02 11:20:45 -0400

    refactor(OtpResponseGraphQLWrapper): Update class JavaDoc.

--- src/main/java/org/opentripplanner/middleware/otp/response/OtpResponseGraphQLWrapper.java
+++ src/main/java/org/opentripplanner/middleware/otp/response/OtpResponseGraphQLWrapper.java
@@ -2,11 +2,7 @@ package org.opentripplanner.middleware.otp.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-/**
- * Represents a trip planner response
- *
- * This is a pared-down version of the org.opentripplanner.api.resource.Response class in OpenTripPlanner.
- */
+/** Wrapper for an OTP response for the GraphQL 'plan' query. */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OtpResponseGraphQLWrapper {
     public OtpResponse data;
