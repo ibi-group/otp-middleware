fe38a33b34a6507d0db5cd7e2177e0ec762ab26c
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-08-05 11:03:32 -0400

    refactor(TripRequest): Remove obsolete `requestParameters` field.

--- src/main/java/org/opentripplanner/middleware/models/TripRequest.java
+++ src/main/java/org/opentripplanner/middleware/models/TripRequest.java
@@ -5,10 +5,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.mongodb.client.FindIterable;
 import org.opentripplanner.middleware.otp.graphql.QueryVariables;
 import org.opentripplanner.middleware.persistence.Persistence;
+import org.opentripplanner.middleware.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
 
 import static com.mongodb.client.model.Filters.eq;
 import static org.opentripplanner.middleware.persistence.TypedPersistence.filterByUserId;
@@ -43,9 +42,9 @@ public class TripRequest extends Model {
      */
     public String toPlace;
 
-    /** A dictionary of the parameters provided in the request that triggered this response. */
-    public Map<String, String> requestParameters;
-
+    /**
+     * The variables passed to the OTP GraphQL `plan` request that triggered this response.
+     */
     public QueryVariables otp2QueryParams;
 
     /**
@@ -73,7 +72,7 @@ public class TripRequest extends Model {
             ", batchId='" + batchId + '\'' +
             ", fromPlace='" + fromPlace + '\'' +
             ", toPlace='" + toPlace + '\'' +
-            ", requestParameters=" + requestParameters +
+            ", otp2QueryParams=" + JsonUtils.toJson(otp2QueryParams) +
             '}';
     }
 
