42b2896b935317d2043f2a348c59996f3293c9e9
Robin Beer <robin.beer@arcadis.com>
2024-10-10 09:01:42 +0100

    refactor(Updated to load plan query from resource URI): Now downloads the query from a public repo o

--- src/main/java/org/opentripplanner/middleware/utils/GraphQLUtils.java
+++ src/main/java/org/opentripplanner/middleware/utils/GraphQLUtils.java
@@ -1,52 +1,68 @@
 package org.opentripplanner.middleware.utils;
 
+import org.eclipse.jetty.http.HttpMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.net.URI;
+
+import static org.opentripplanner.middleware.utils.ConfigUtils.getConfigPropertyAsText;
 
 public class GraphQLUtils {
+
+    private GraphQLUtils() {
+        throw new IllegalStateException("Utility class.");
+    }
+
     private static final Logger LOG = LoggerFactory.getLogger(GraphQLUtils.class);
 
     // Lazily-initialized in getPlanQueryTemplate()
     private static String planQueryTemplate = null;
 
     /**
-     * Location of the GraphQL plan query template file, as Java resource.
+     * Location of the GraphQL plan query template file, as URI resource.
      */
-    public static final String PLAN_QUERY_RESOURCE = "queries/planQuery.graphql";
+    private static final String PLAN_QUERY_RESOURCE_URI =
+        getConfigPropertyAsText("PLAN_QUERY_RESOURCE_URI", null);
+
 
     /**
-     * Return the full GraphQL plan file planQueryTemplate in Java string format, with {@code "} as {@code \"}
+     * Return the full GraphQL plan file planQueryTemplate.
      */
     public static String getPlanQueryTemplate() {
         if (GraphQLUtils.planQueryTemplate == null) {
-            GraphQLUtils.planQueryTemplate = planQueryTemplateAsString(PLAN_QUERY_RESOURCE);
-	}
+            GraphQLUtils.planQueryTemplate = planQueryTemplateAsString();
+        }
         return GraphQLUtils.planQueryTemplate;
     }
 
     /**
-     * Return a GraphQL planQueryTemplate in Java string format, with {@code "} as {@code \"}
-     * @param resource the plan file or any GraphQL file
+     * Return a GraphQL planQueryTemplate in Java string format, with {@code "} as {@code \"}.
      */
-    static String planQueryTemplateAsString(String resource) {
-        StringBuilder builder = new StringBuilder();
-        try (var reader = new BufferedReader(new InputStreamReader(
-            GraphQLUtils.class.getClassLoader().getResourceAsStream(resource)
-        ))) {
-            int value;
-            // All this low-level stuff is just to put a \ in front of " in the string.
-            while ((value = reader.read()) != -1) {
-                char c = (char)value;
-                if (c == '\n') builder.append("\\n");
-                else if (c == '"') builder.append("\\\"");
-                else builder.append(c);
-            }
-        } catch (Exception e) {
-            LOG.error("Can't find \"{}\" resource.", resource, e);
+    static String planQueryTemplateAsString() {
+        String rawPlanQuery = getPlanQueryFromResource();
+        if (rawPlanQuery == null) {
+            LOG.error("Unable to retrieve plan query from resource: {}.", GraphQLUtils.PLAN_QUERY_RESOURCE_URI);
+            return null;
         }
-        return builder.toString();
+        return rawPlanQuery.replace("\"", "\\\"");
+    }
+
+    /**
+     * Download the plan query from resource URI.
+     */
+    static String getPlanQueryFromResource() {
+        if (GraphQLUtils.PLAN_QUERY_RESOURCE_URI == null) {
+            LOG.error("The plan query resource URI parameter \"PLAN_QUERY_RESOURCE_URI\" is undefined.");
+            return null;
+        }
+        HttpResponseValues httpResponseValues = HttpUtils.httpRequestRawResponse(
+            URI.create(GraphQLUtils.PLAN_QUERY_RESOURCE_URI),
+            10,
+            HttpMethod.GET,
+            null,
+            null
+        );
+        return httpResponseValues != null ? httpResponseValues.responseBody : null;
     }
 }
