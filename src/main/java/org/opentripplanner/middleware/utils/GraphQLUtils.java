4539fc0123af8deee15cdd5ac87a6acc0c050a72
Robin Beer <robin.beer@arcadis.com>
2024-10-14 11:23:33 +0100

    refactor(Various updates to address PR feedback):

--- src/main/java/org/opentripplanner/middleware/utils/GraphQLUtils.java
+++ src/main/java/org/opentripplanner/middleware/utils/GraphQLUtils.java
@@ -1,6 +1,7 @@
 package org.opentripplanner.middleware.utils;
 
 import org.eclipse.jetty.http.HttpMethod;
+import org.opentripplanner.middleware.bugsnag.BugsnagReporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,12 +21,18 @@ public class GraphQLUtils {
     private static String planQueryTemplate = null;
 
     /**
+     * Location of the GraphQL default plan query template file, as URI resource.
+     */
+    private static final String DEFAULT_PLAN_QUERY_RESOURCE_URI =
+        "https://raw.githubusercontent.com/opentripplanner/otp-ui/refs/heads/master/packages/core-utils/src/planQuery.graphql";
+
+    /**
      * Location of the GraphQL plan query template file, as URI resource.
      */
     private static final String PLAN_QUERY_RESOURCE_URI =
         getConfigPropertyAsText(
             "PLAN_QUERY_RESOURCE_URI",
-            "https://raw.githubusercontent.com/opentripplanner/otp-ui/refs/heads/master/packages/core-utils/src/planQuery.graphql"
+            DEFAULT_PLAN_QUERY_RESOURCE_URI
         );
 
 
@@ -45,8 +52,9 @@ public class GraphQLUtils {
     static String planQueryTemplateAsString() {
         String rawPlanQuery = getPlanQueryFromResource();
         if (rawPlanQuery == null) {
-            LOG.error("Unable to retrieve plan query from resource: {}.", GraphQLUtils.PLAN_QUERY_RESOURCE_URI);
-            return null;
+            String message = String.format("Unable to retrieve plan query from resource: %s.", GraphQLUtils.PLAN_QUERY_RESOURCE_URI);
+            LOG.error(message);
+            throw new IllegalStateException(message);
         }
         return rawPlanQuery.replace("\"", "\\\"");
     }
@@ -55,10 +63,6 @@ public class GraphQLUtils {
      * Download the plan query from resource URI.
      */
     static String getPlanQueryFromResource() {
-        if (GraphQLUtils.PLAN_QUERY_RESOURCE_URI == null) {
-            LOG.error("The plan query resource URI parameter \"PLAN_QUERY_RESOURCE_URI\" is undefined.");
-            return null;
-        }
         HttpResponseValues httpResponseValues = HttpUtils.httpRequestRawResponse(
             URI.create(GraphQLUtils.PLAN_QUERY_RESOURCE_URI),
             10,
