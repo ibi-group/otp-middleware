22423f881f235d596707bdd3b7445bac45dc1c8d
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-10-29 09:20:27 -0400

    refactor(ItineraryExistence): Tweak error message for trip not possible.

--- src/main/java/org/opentripplanner/middleware/models/ItineraryExistence.java
+++ src/main/java/org/opentripplanner/middleware/models/ItineraryExistence.java
@@ -242,7 +242,7 @@ public class ItineraryExistence extends Model {
         }
         if (!allMonitoredDaysAreValid(trip)) {
             this.message = String.format(
-                "The trip is not possible on the following days of the week you have selected: %s. Real-time conditions might have changed since you last planned this trip, so try returning to the trip planner, perform the itinerary search again, and save the result.",
+                "The trip is not possible on the following days of the week you have selected: %s. Real-time conditions have changed since this trip was planned. Return to the trip planner, plan a new trip, and save the result.",
                 getInvalidDaysOfWeekMessage()
             );
             this.error = true;
