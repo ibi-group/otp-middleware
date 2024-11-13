733eddaa109a46e96b55f45dc24718c4a085d8d9
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-11-13 10:50:41 -0500

    refactor(typeform.Form): Introduce typeform package with Form response.

---
+++ src/main/java/org/opentripplanner/middleware/models/typeform/Field.java
@@ -0,0 +1,20 @@
+package org.opentripplanner.middleware.models.typeform;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/** Relevant fields info in surveys. */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Field {
+    public String id;
+
+    public String title;
+
+    public Field() {
+        // Empty constructor for deserialization
+    }
+
+    public Field(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}
---
+++ src/main/java/org/opentripplanner/middleware/models/typeform/Form.java
@@ -0,0 +1,26 @@
+package org.opentripplanner.middleware.models.typeform;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/** Data structure for relevant TypeForm Form data. */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Form {
+
+    public List<Field> fields;
+
+    public List<String> hidden;
+
+    public String toCsvHeader() {
+        List<String> standardHeaders = List.of("id", "status", "started", "completed");
+        List<String> allFields = new ArrayList<>();
+        allFields.addAll(standardHeaders);
+        allFields.addAll(hidden);
+        allFields.addAll(fields.stream().map(f -> String.format("\"%s\"", f.title)).collect(Collectors.toList()));
+
+        return String.join(",", allFields);
+    }
+}
