d746c8d93afb3d2dc2ed39969a4c45903b98c62b
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-11-14 11:53:58 -0500

    refactor(TripHistoryUpload): Remove redundant code

--- src/main/java/org/opentripplanner/middleware/models/TripHistoryUpload.java
+++ src/main/java/org/opentripplanner/middleware/models/TripHistoryUpload.java
@@ -1,10 +1,6 @@
 package org.opentripplanner.middleware.models;
 
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Sorts;
 import org.bson.codecs.pojo.annotations.BsonIgnore;
-import org.bson.conversions.Bson;
-import org.opentripplanner.middleware.connecteddataplatform.IntervalUploadStatus;
 import org.opentripplanner.middleware.persistence.Persistence;
 
 import java.time.LocalDateTime;
@@ -31,19 +27,6 @@ public class TripHistoryUpload extends IntervalUpload {
      */
     @BsonIgnore
     public static TripHistoryUpload getFirst() {
-        return getOneOrdered(Sorts.ascending("dateCreated"));
-    }
-
-    /**
-     * Get one upload based on the sort order.
-     */
-    private static TripHistoryUpload getOneOrdered(Bson sortBy) {
-        return Persistence.tripHistoryUploads.getOneFiltered(
-            Filters.or(
-                Filters.eq("status", IntervalUploadStatus.COMPLETED.getValue()),
-                Filters.eq("status", IntervalUploadStatus.PENDING.getValue())
-            ),
-            sortBy
-        );
+        return IntervalUpload.getFirst(Persistence.tripHistoryUploads);
     }
 }
