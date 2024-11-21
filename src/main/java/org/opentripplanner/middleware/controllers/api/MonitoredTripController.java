a7b20821b077c9f6e0c8873774993b33e247aeb1
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-11-21 09:06:56 -0500

    refactor(MonitoredTripController): Remove unused endpoint, update swagger.

--- src/main/java/org/opentripplanner/middleware/controllers/api/MonitoredTripController.java
+++ src/main/java/org/opentripplanner/middleware/controllers/api/MonitoredTripController.java
@@ -5,16 +5,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mongodb.client.model.Filters;
 import org.bson.conversions.Bson;
 import org.eclipse.jetty.http.HttpStatus;
-import org.opentripplanner.middleware.auth.Auth0Connection;
-import org.opentripplanner.middleware.auth.RequestingUser;
-import org.opentripplanner.middleware.controllers.response.ResponseList;
 import org.opentripplanner.middleware.models.ItineraryExistence;
 import org.opentripplanner.middleware.models.MonitoredTrip;
 import org.opentripplanner.middleware.models.OtpUser;
 import org.opentripplanner.middleware.persistence.Persistence;
 import org.opentripplanner.middleware.tripmonitor.jobs.CheckMonitoredTrip;
 import org.opentripplanner.middleware.tripmonitor.jobs.MonitoredTripLocks;
-import org.opentripplanner.middleware.utils.HttpUtils;
 import org.opentripplanner.middleware.utils.InvalidItineraryReason;
 import org.opentripplanner.middleware.utils.JsonUtils;
 import org.opentripplanner.middleware.utils.SwaggerUtils;
@@ -48,17 +44,6 @@ public class MonitoredTripController extends ApiController<MonitoredTrip> {
     protected void buildEndpoint(ApiEndpoint baseEndpoint) {
         // Add the api key route BEFORE the regular CRUD methods
         ApiEndpoint modifiedEndpoint = baseEndpoint
-            // Get all trips, including shared trips created by others.
-            .get(path(ROOT_ROUTE + "/gettrips")
-                    .withDescription(
-                        "Gets a paginated list of trips created by the current user and shared trips created by others where the current user is the primary traveler or a companion or an observer."
-                    )
-                    .withQueryParam(LIMIT)
-                    .withQueryParam(OFFSET)
-                    .withProduces(HttpUtils.JSON_ONLY)
-                    .withResponseType(ResponseList.class),
-                this::getTrips, JsonUtils::toJson
-            )
             .post(path("/checkitinerary")
                     .withDescription("Returns the itinerary existence check results for a monitored trip.")
                     .withRequestType(MonitoredTrip.class)
@@ -77,21 +62,6 @@ public class MonitoredTripController extends ApiController<MonitoredTrip> {
             Filters.eq("companion.email", user.email),
             Filters.eq("observers.email", user.email)
         );
-    }
-
-    private ResponseList<MonitoredTrip> getTrips(Request req, Response res) {
-        int limit = HttpUtils.getQueryParamFromRequest(req, LIMIT_PARAM, 0, DEFAULT_LIMIT, 100);
-        int offset = HttpUtils.getQueryParamFromRequest(req, OFFSET_PARAM, 0, DEFAULT_OFFSET);
-        RequestingUser requestingUser = Auth0Connection.getUserFromRequest(req);
-
-        String userId = requestingUser.otpUser.id;
-        Bson finalFilter = Filters.or(
-            Filters.eq(USER_ID_PARAM, userId),
-            Filters.eq("primary.userId", userId),
-            Filters.eq("companion.email", requestingUser.otpUser.email),
-            Filters.eq("observers.email", requestingUser.otpUser.email)
-        );
-        return persistence.getResponseList(finalFilter, offset, limit);
     }
 
     /**
