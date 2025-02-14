b99ecce0f968731e8a01301245efab55b709872f
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2025-02-14 16:23:26 -0500

    test(ManageLegTraversal): Use builder syntax with TraceData, remove old code.

--- src/test/java/org/opentripplanner/middleware/triptracker/TraceData.java
+++ src/test/java/org/opentripplanner/middleware/triptracker/TraceData.java
@@ -13,51 +13,47 @@ class TraceData {
     boolean isStartOfTrip;
     boolean dismissIntermediateStops;
     Instant instant;
-    String message;
 
-    public TraceData(Coordinates position, String expectedInstruction, boolean isStartOfTrip, String message) {
+    public TraceData withPosition(Coordinates position) {
         this.position = position;
-        this.expectedInstruction = expectedInstruction;
-        this.isStartOfTrip = isStartOfTrip;
-        this.message = message;
+        return this;
     }
 
-    public TraceData(Coordinates position, TripInstruction expectedInstruction, boolean isStartOfTrip, String message) {
-        this(position, expectedInstruction.build(), isStartOfTrip, message);
+    public TraceData withPosition(double lat, double lon) {
+        return withPosition(new Coordinates(lat, lon));
     }
 
-    public TraceData(Coordinates position, String expectedInstruction, String message) {
-        this(position, expectedInstruction, false, message);
+    public TraceData withExpectedInstruction(String instruction) {
+        this.expectedInstruction = instruction;
+        return this;
     }
 
-    public TraceData(Coordinates position, String expectedInstruction, Instant instant, String message) {
-        this(position, expectedInstruction, false, message);
-        this.instant = instant;
+    public TraceData withExpectedInstruction(TripInstruction instruction) {
+        return withExpectedInstruction(instruction.build());
     }
 
-    public TraceData(Coordinates position, String expectedInstruction, String message, boolean dismissIntermediateStops) {
-        this(position, expectedInstruction, false, message);
-        this.dismissIntermediateStops = dismissIntermediateStops;
-    }
-
-    public TraceData(Coordinates position, int speed, String expectedInstruction, String message) {
-        this(position, expectedInstruction, false, message);
+    public TraceData withSpeed(int speed) {
         this.speed = speed;
+        return this;
     }
 
-    public TraceData(TripStatus tripStatus, Coordinates position, String expectedInstruction, boolean isStartOfTrip, String message) {
+    public TraceData withInstant(Instant instant) {
+        this.instant = instant;
+        return this;
+    }
+
+    public TraceData withNullIntermediateStops() {
+        this.dismissIntermediateStops = true;
+        return this;
+    }
+
+    public TraceData withStartingTrip() {
+        this.isStartOfTrip = true;
+        return this;
+    }
+
+    public TraceData withTripStatus(TripStatus tripStatus) {
         this.tripStatus = tripStatus;
-        this.position = position;
-        this.expectedInstruction = expectedInstruction;
-        this.isStartOfTrip = isStartOfTrip;
-        this.message = message;
-    }
-
-    public TraceData(TripStatus tripStatus, Coordinates position, TripInstruction expectedInstruction, boolean isStartOfTrip, String message) {
-        this(tripStatus, position, expectedInstruction.build(), isStartOfTrip, message);
-    }
-
-    public TraceData(TripStatus tripStatus, Coordinates position, String expectedInstruction, String message) {
-        this(tripStatus, position, expectedInstruction, false, message);
+        return this;
     }
 }
