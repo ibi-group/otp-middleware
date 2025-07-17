c32be971621efb37c7549951ce38b972df7011d2
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2025-07-17 16:14:51 -0400

    fix(WaitForTransitInstruction): Add indication if no real-time info.

--- src/main/java/org/opentripplanner/middleware/triptracker/instruction/WaitForTransitInstruction.java
+++ src/main/java/org/opentripplanner/middleware/triptracker/instruction/WaitForTransitInstruction.java
@@ -36,11 +36,13 @@ public class WaitForTransitInstruction extends TransitLegInstruction {
         String waitInfo;
         if (waitInMinutes < -2) {
             waitInfo = " (That time has passed)";
-        } else {
+        } else if (Boolean.TRUE.equals(transitLeg.realTime)) {
             String delayInfo = (delayInMinutes > 0) ? "late" : "early";
             waitInfo = (absoluteMinutes <= 1)
                 ? ", on time"
                 : String.format(" now%s %s", getReadableMinutes(delayInMinutes), delayInfo);
+        } else {
+            waitInfo = " (No real-time info)";
         }
         return String.format(
             "Wait%s for your bus, route %s, scheduled at %s%s",
