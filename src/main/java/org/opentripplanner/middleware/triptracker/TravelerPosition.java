4a92fcb9ba5646495a61c80ffea9ddc6b4a381d2
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-09-12 10:40:22 -0400

    fix(TravelerLocator): Direct traveler to next leg if in gap between transit and walk legs.

--- src/main/java/org/opentripplanner/middleware/triptracker/TravelerPosition.java
+++ src/main/java/org/opentripplanner/middleware/triptracker/TravelerPosition.java
@@ -93,20 +93,27 @@ public class TravelerPosition {
      * Returns the closest transit leg in 'upcoming' radius eligible for a "Wait for transit" instruction.
      */
     public Leg getTransitLegWithClosestUpcomingOrigin() {
-        double distancetoExpectedLeg = distanceToTransitLegOrigin(currentPosition, expectedLeg);
-        double distanceToNextLeg = distanceToTransitLegOrigin(currentPosition, nextLeg);
+        return getLegWithClosestUpcomingOrigin(true);
+    }
 
-        if (distancetoExpectedLeg <= TRIP_INSTRUCTION_UPCOMING_RADIUS && distancetoExpectedLeg < distanceToNextLeg) {
+    /**
+     * Returns the closest leg in 'upcoming' radius.
+     */
+    public Leg getLegWithClosestUpcomingOrigin(boolean isTransit) {
+        double distanceToExpectedLeg = distanceToLegOrigin(currentPosition, expectedLeg, isTransit);
+        double distanceToNextLeg = distanceToLegOrigin(currentPosition, nextLeg, isTransit);
+
+        if (distanceToExpectedLeg <= TRIP_INSTRUCTION_UPCOMING_RADIUS && distanceToExpectedLeg < distanceToNextLeg) {
             return expectedLeg;
-        } else if (distanceToNextLeg <= TRIP_INSTRUCTION_UPCOMING_RADIUS && distanceToNextLeg < distancetoExpectedLeg) {
+        } else if (distanceToNextLeg <= TRIP_INSTRUCTION_UPCOMING_RADIUS && distanceToNextLeg < distanceToExpectedLeg) {
             return nextLeg;
         } else {
             return null;
         }
     }
 
-    private static double distanceToTransitLegOrigin(Coordinates position, Leg leg) {
-        return leg != null && leg.transitLeg && leg.from != null
+    private static double distanceToLegOrigin(Coordinates position, Leg leg, boolean isTransit) {
+        return leg != null && (!isTransit || leg.transitLeg) && leg.from != null
             ? getDistance(position, leg.from.toCoordinates())
             : Double.MAX_VALUE;
     }
