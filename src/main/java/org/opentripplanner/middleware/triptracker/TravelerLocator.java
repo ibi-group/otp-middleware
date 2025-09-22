f6f97a7e921a4191a84002d71d0a4e53f29c3a5f
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-09-22 15:08:22 -0400

    fix(TravelerLocator): Correctly compute bus departure time

--- src/main/java/org/opentripplanner/middleware/triptracker/TravelerLocator.java
+++ src/main/java/org/opentripplanner/middleware/triptracker/TravelerLocator.java
@@ -407,7 +407,7 @@ public class TravelerLocator {
      */
     public static Instant getBusDepartureTime(Leg busLeg) {
         return ZonedDateTime.ofInstant(
-            busLeg.startTime.toInstant().plusSeconds(busLeg.departureDelay),
+            busLeg.startTime.toInstant(), // already includes any delays
             DateTimeUtils.getOtpZoneId()
         ).toInstant();
     }
