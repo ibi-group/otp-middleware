c00dbaaaffe4b09d379c9e695d467d3b031c9ea2
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-10-23 17:24:13 -0400

    feat(DailyStats): Add basic DailyStats class and persistence.

---
+++ src/main/java/org/opentripplanner/middleware/stats/DailyStats.java
@@ -0,0 +1,36 @@
+package org.opentripplanner.middleware.stats;
+
+import org.opentripplanner.middleware.models.Model;
+
+import java.time.LocalDate;
+
+/**
+ * Holds anonymous statistics collected on a daily basis.
+ */
+public class DailyStats extends Model {
+    /**
+     * The date for which the stats apply.
+     */
+    public LocalDate date;
+
+    /**
+     * The total number of {@link org.opentripplanner.middleware.models.OtpUser}
+     * at the time the statistics are collected.
+     */
+    public int otpUsers;
+
+    /**
+     * The number of {@link org.opentripplanner.middleware.models.TripRequest} by logged-in users
+     * for the date the statistics are collected.
+     */
+    public int tripRequests;
+
+    /**
+     * The number of {@link org.opentripplanner.middleware.models.OtpUser} associated with the
+     * {@link org.opentripplanner.middleware.models.TripRequest}
+     * for the date the statistics are collected.
+     */
+    public int otpUsersWithTripRequests;
+
+    // Insert other stats here as needed.
+}
