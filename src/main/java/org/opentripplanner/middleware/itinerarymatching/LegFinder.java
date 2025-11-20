e26206cd3e1a2e500e80ee574bef00e3a6d0fc34
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-11-20 15:44:10 -0500

    feat(LegFinder): Add LegFinder class to make OTP leg queries.

---
+++ src/main/java/org/opentripplanner/middleware/itinerarymatching/LegFinder.java
@@ -0,0 +1,79 @@
+package org.opentripplanner.middleware.itinerarymatching;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.opentripplanner.middleware.otp.OtpDispatcherResponse;
+import org.opentripplanner.middleware.otp.OtpGraphQLQuery;
+import org.opentripplanner.middleware.otp.OtpVersion;
+import org.opentripplanner.middleware.otp.response.Leg;
+import org.opentripplanner.middleware.utils.HttpUtils;
+import org.opentripplanner.middleware.utils.JsonUtils;
+
+import java.util.function.Function;
+
+import static org.opentripplanner.middleware.otp.OtpDispatcher.OTP_GRAPHQL_ENDPOINT;
+import static org.opentripplanner.middleware.otp.OtpDispatcher.sendOtpPostRequest;
+
+/**
+ * Helper class to perform a leg query in OTP.
+ */
+public class LegFinder {
+    private final Function<String, OtpDispatcherResponse> legResponseProvider;
+
+    public LegFinder(Function<String, OtpDispatcherResponse> legResponseProvider) {
+        this.legResponseProvider = legResponseProvider;
+    }
+
+    public LegFinder() {
+        this(LegFinder::sendOtpLegRequest);
+    }
+
+    private Leg queryLeg(String legId) {
+        OtpDispatcherResponse dispResponse = legResponseProvider.apply(legId);
+        if (dispResponse != null && dispResponse.statusCode < 400) {
+            try {
+                return JsonUtils.getPOJOFromJSON(dispResponse.responseBody, LegResponseWrapper.class).data.leg;
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
+    }
+
+    public boolean legExists(String legId) {
+        return queryLeg(legId) != null;
+    }
+
+    /**
+     * Provides OTP's response for the desired leg id.
+     */
+    // TODO: Adjust OtpDispatcherResponseType
+    public static OtpDispatcherResponse sendOtpLegRequest(String legId) {
+        OtpGraphQLQuery<LegQueryVariables> query = new OtpGraphQLQuery<>();
+        query.query = "query ($legId: String!) { leg(id: $legId) { id } }";
+        query.variables = new LegQueryVariables(legId);
+        return sendOtpPostRequest(
+            OtpVersion.OTP2,
+            "",
+            OTP_GRAPHQL_ENDPOINT,
+            HttpUtils.HEADERS_JSON,
+            JsonUtils.toJson(query).replace("\\\\n", "\\n").replace("\\\\\"", "\"")
+        );
+    }
+
+    public static class LegQueryVariables {
+        public final String legId;
+
+        public LegQueryVariables(String legId) {
+            this.legId = legId;
+        }
+    }
+
+    // TODO: Combine with OtpResponse
+    public static class LegResponseWrapper {
+        public LegResponse data;
+    }
+
+    public static class LegResponse {
+        public Leg leg;
+    }
+}
---
+++ src/test/java/org/opentripplanner/middleware/itinerarymatching/LegFinderTest.java
@@ -0,0 +1,31 @@
+package org.opentripplanner.middleware.itinerarymatching;
+
+import org.eclipse.jetty.http.HttpStatus;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.opentripplanner.middleware.otp.OtpDispatcherResponse;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LegFinderTest {
+    @ParameterizedTest
+    @MethodSource("legExistsCases")
+    void existingLeg(String responseBody, boolean expected) {
+        OtpDispatcherResponse otpDispatcherResponse = new OtpDispatcherResponse();
+        otpDispatcherResponse.statusCode = HttpStatus.OK_200;
+        otpDispatcherResponse.responseBody = responseBody;
+        LegFinder legFinder = new LegFinder(ignored -> otpDispatcherResponse);
+        assertEquals(expected, legFinder.legExists("leg-id"));
+    }
+
+    private static Stream<Arguments> legExistsCases() {
+        return Stream.of(
+            Arguments.of("{ \"data\": { \"leg\": { \"id\": \"leg-id\" } } }", true),
+            Arguments.of("{ \"data\": { \"leg\": null } }", false)
+        );
+    }
+
+}
