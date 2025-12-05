d3f5aacf6b501877d7ea6e0e4c07bef52bd78f05
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-12-05 13:29:32 -0500

    test(ItineraryFromLegMatcher): Convert to parameterized test.

--- src/test/java/org/opentripplanner/middleware/itinerarymatching/ItineraryFromLegMatcherTest.java
+++ src/test/java/org/opentripplanner/middleware/itinerarymatching/ItineraryFromLegMatcherTest.java
@@ -1,37 +1,43 @@
 package org.opentripplanner.middleware.itinerarymatching;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.opentripplanner.middleware.otp.response.Itinerary;
 import org.opentripplanner.middleware.otp.response.Leg;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.opentripplanner.middleware.itinerarymatching.ItineraryMatchingUtils.createBusLeg;
 import static org.opentripplanner.middleware.itinerarymatching.ItineraryMatchingUtils.createTransitWalkTransitItinerary;
 
 class ItineraryFromLegMatcherTest {
-    @Test
-    void canMatchItineraryFromLegs() {
-        LocalDateTime baseTime = LocalDateTime.of(2025, 11, 10, 8, 0, 0);
 
-        Itinerary itinerary = createTransitWalkTransitItinerary(baseTime);
+    public static final LocalDateTime BASE_TIME = LocalDateTime.of(2025, 11, 10, 8, 0, 0);
 
+    @ParameterizedTest
+    @MethodSource("itineraryFromLegsCases")
+    void canMatchItineraryFromLegs(Collection<Leg> legs, boolean isMatch, String message) {
+        Itinerary itinerary = createTransitWalkTransitItinerary(BASE_TIME);
+        ItineraryFromLegMatcher matcher = new ItineraryFromLegMatcher(itinerary, legs);
+        assertEquals(isMatch, matcher.match(), message);
+    }
+
+    private static Stream<Arguments> itineraryFromLegsCases() {
         // Set up live (real-time) transit legs.
         // Non-null transit legs are presumed to match origin, destination, and trip id on a given transit route.
         // TODO: Handle cases with different leg start/end times (delays), and cases where some legs are null.
-        Leg liveLeg1 = createBusLeg("transit-leg-id-1", baseTime, baseTime.plusMinutes(10));
-        Leg liveLeg2 = createBusLeg("transit-leg-id-2", baseTime.plusMinutes(40), baseTime.plusMinutes(50));
+        Leg liveLeg1 = createBusLeg("transit-leg-id-1", BASE_TIME, BASE_TIME.plusMinutes(10));
+        Leg liveLeg2 = createBusLeg("transit-leg-id-2", BASE_TIME.plusMinutes(40), BASE_TIME.plusMinutes(50));
 
-        ItineraryFromLegMatcher matcher = new ItineraryFromLegMatcher(itinerary, List.of(liveLeg1, liveLeg2));
-        assertTrue(matcher.match(), "Transit legs in order should match.");
-
-        ItineraryFromLegMatcher matcher1 = new ItineraryFromLegMatcher(itinerary, List.of(liveLeg2, liveLeg1));
-        assertFalse(matcher1.match(), "Transit legs out of order should not match.");
-
-        ItineraryFromLegMatcher matcher2 = new ItineraryFromLegMatcher(itinerary, List.of(liveLeg1));
-        assertFalse(matcher2.match(),"Missing transit legs should not match.");
+        return Stream.of(
+            Arguments.of(List.of(liveLeg1, liveLeg2), true, "Transit legs in order should match."),
+            Arguments.of(List.of(liveLeg2, liveLeg1), false, "Transit legs out of order should not match."),
+            Arguments.of(List.of(liveLeg1), false, "Missing transit legs should not match.")
+        );
     }
 }
