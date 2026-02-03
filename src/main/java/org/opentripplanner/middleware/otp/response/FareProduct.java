47af602400e3b8ca1ac273a83325ed39bf66c791
danielhep <dh@danielheppner.com>
2026-02-03 15:02:54 -0800

    save faretable

---
+++ src/main/java/org/opentripplanner/middleware/otp/response/FareProduct.java
@@ -0,0 +1,23 @@
+package org.opentripplanner.middleware.otp.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.bson.codecs.pojo.annotations.BsonIgnore;
+
+import java.util.Collection;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FareProduct implements Cloneable {
+
+    // __typename
+    public /*FeedScopedId*/ String id;
+    public FareMedium medium;
+    public String name;
+    public RiderCategory riderCategory;
+    public Money price;
+    @JsonIgnore
+    @BsonIgnore
+    Collection<FareProduct> dependencies;
+}
