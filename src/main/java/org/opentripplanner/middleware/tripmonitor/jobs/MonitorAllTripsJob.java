0e8ba0d57827d04aadc0260f5d0d5688f298e7e6
binh-dam-ibigroup <binh.dam@arcadis.com>
2026-04-28 18:25:52 -0400

    perf(MonitorAllTripsJob): Filter out skipped trips

--- src/main/java/org/opentripplanner/middleware/tripmonitor/jobs/MonitorAllTripsJob.java
+++ src/main/java/org/opentripplanner/middleware/tripmonitor/jobs/MonitorAllTripsJob.java
@@ -1,8 +1,9 @@
 package org.opentripplanner.middleware.tripmonitor.jobs;
 
-import com.mongodb.BasicDBObject;
+import org.bson.conversions.Bson;
 import org.opentripplanner.middleware.persistence.Persistence;
 import org.opentripplanner.middleware.recurringjobs.RecurringJobScheduler;
+import org.opentripplanner.middleware.tripmonitor.TripStatus;
 import org.opentripplanner.middleware.utils.Scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,10 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.ne;
+import static com.mongodb.client.model.Filters.nor;
 import static org.opentripplanner.middleware.controllers.api.ApiController.ID_FIELD_NAME;
 
 /**
@@ -109,18 +114,30 @@ public class MonitorAllTripsJob implements Runnable, RecurringJobScheduler {
     }
 
     /**
-     * Create a BSON clause to filter out trips that would not be checked.
+     * Create a BSON clause to filter out trips that should not be checked.
      */
-    private static BasicDBObject makeTripFilter() {
-        BasicDBObject tripFilter = new BasicDBObject();
-
-        //.Trips must be active.
-        tripFilter.put("isActive", true);
-
-        // Other conditions (e.g. in CheckMonitoredTrip) that would result in a trip to be not checked
-        // should eventually be moved here.
-
-        return tripFilter;
+    public static Bson makeTripFilter() {
+        return and(
+            // Trips must be active.
+            eq("isActive", true),
+            // If trip is no longer possible, no further checking is needed.
+            ne("journeyState.tripStatus", TripStatus.NO_LONGER_POSSIBLE.name()),
+            // Exclude one-time trips in the past
+            nor(
+                and(
+                    eq("journeyState.tripStatus", TripStatus.PAST_TRIP.name()),
+                    eq("monday", false),
+                    eq("tuesday", false),
+                    eq("wednesday", false),
+                    eq("thursday", false),
+                    eq("friday", false),
+                    eq("saturday", false),
+                    eq("sunday", false)
+                )
+            )
+            // Other conditions (e.g. in CheckMonitoredTrip) that would result in a trip to be not checked
+            // should eventually be moved here.
+        );
     }
 
     /**
---
+++ src/test/java/org/opentripplanner/middleware/tripmonitor/jobs/MonitorAllTripsJobTest.java
@@ -0,0 +1,93 @@
+package org.opentripplanner.middleware.tripmonitor.jobs;
+
+import org.bson.conversions.Bson;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.opentripplanner.middleware.models.MonitoredTrip;
+import org.opentripplanner.middleware.models.OtpUser;
+import org.opentripplanner.middleware.persistence.Persistence;
+import org.opentripplanner.middleware.testutils.OtpMiddlewareTestEnvironment;
+import org.opentripplanner.middleware.testutils.OtpTestUtils;
+import org.opentripplanner.middleware.testutils.PersistenceTestUtils;
+import org.opentripplanner.middleware.tripmonitor.JourneyState;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.opentripplanner.middleware.tripmonitor.TripStatus.NO_LONGER_POSSIBLE;
+import static org.opentripplanner.middleware.tripmonitor.TripStatus.PAST_TRIP;
+
+/**
+ * This class contains tests for the {@link MonitorAllTripsJob} class.
+ */
+class MonitorAllTripsJobTest extends OtpMiddlewareTestEnvironment {
+    private static OtpUser user;
+
+    @BeforeAll
+    static void setup() {
+        user = PersistenceTestUtils.createUser("user@example.com");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        user.delete(false);
+    }
+
+    @Test
+    void canFilterOutTrips() throws Exception {
+        // Default active trip.
+        PersistenceTestUtils.createMonitoredTrip(
+            user.id,
+            OtpTestUtils.OTP2_DISPATCHER_PLAN_RESPONSE,
+            true,
+            null
+        );
+        Bson userFilter = eq("userId", user.id);
+        MonitoredTrip activeTrip = Persistence.monitoredTrips.getOneFiltered(userFilter);
+
+        MonitoredTrip inactiveTrip = createInactiveTrip();
+        Persistence.monitoredTrips.create(inactiveTrip);
+
+        MonitoredTrip obsoleteTrip = createObsoleteTrip();
+        Persistence.monitoredTrips.create(obsoleteTrip);
+
+        MonitoredTrip oneTimePastTrip = createOneTimePastTrip();
+        Persistence.monitoredTrips.create(oneTimePastTrip);
+
+        Bson filter = and(
+            MonitorAllTripsJob.makeTripFilter(),
+            userFilter
+        );
+
+        var trips = Persistence.monitoredTrips.getResponseList(filter, 0, 100);
+        assertEquals(1, trips.data.size());
+        assertEquals(activeTrip.id, trips.data.get(0).id);
+    }
+
+    private static MonitoredTrip createOneTimePastTrip() {
+        MonitoredTrip trip = new MonitoredTrip();
+        trip.userId = user.id;
+        trip.isActive = true;
+        trip.journeyState = new JourneyState();
+        trip.journeyState.tripStatus = PAST_TRIP;
+        trip.updateAllDaysOfWeek(false);
+        return trip;
+    }
+
+    private static MonitoredTrip createObsoleteTrip() {
+        MonitoredTrip trip = new MonitoredTrip();
+        trip.userId = user.id;
+        trip.isActive = true;
+        trip.journeyState = new JourneyState();
+        trip.journeyState.tripStatus = NO_LONGER_POSSIBLE;
+        return trip;
+    }
+
+    private static MonitoredTrip createInactiveTrip() {
+        MonitoredTrip trip = new MonitoredTrip();
+        trip.userId = user.id;
+        trip.isActive = false;
+        return trip;
+    }
+}
