6f23cf6a9485c681e793e930c5a612f0a2c0c6b9
binh-dam-ibigroup <binh.dam@arcadis.com>
2026-08-28 16:00:56 -0400

    fix(FareProductUse): Add JSON-ignore unknown fields.

--- src/main/java/org/opentripplanner/middleware/otp/response/FareProductUse.java
+++ src/main/java/org/opentripplanner/middleware/otp/response/FareProductUse.java
@@ -1,5 +1,8 @@
 package org.opentripplanner.middleware.otp.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FareProductUse {
     public String id;
     public FareProduct product;
