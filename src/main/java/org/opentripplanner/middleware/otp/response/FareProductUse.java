f87790ee0cd3a5913a21165209e23410a838db59
binh-dam-ibigroup <binh.dam@arcadis.com>
2026-09-14 12:37:35 -0400

    refactor(FareProductUse): exclude nulls from serialization.

--- src/main/java/org/opentripplanner/middleware/otp/response/FareProductUse.java
+++ src/main/java/org/opentripplanner/middleware/otp/response/FareProductUse.java
@@ -1,8 +1,10 @@
 package org.opentripplanner.middleware.otp.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FareProductUse {
     public String id;
     public FareProduct product;
